ee9632b7b9130c14884cc47df8caa57edde44bc8
manjeet arneja <manjeet.arneja@gmail.com>
2020-12-22 18:08:10 +0530

    uncommented query from db/org/applications

--- api/src/db/organisation/application.rs
+++ api/src/db/organisation/application.rs
@@ -13,45 +13,45 @@ use crate::{models::db_mapping::
 pub async fn initialize_application_pre(
 	transaction: &mut Transaction<'_, MySql>,
 ) -> Result<(), sqlx::Error> {
-	// log::info!("Initializing application tables");
-	// query!(
-	// 	r#"
-	// 	CREATE TABLE IF NOT EXISTS application (
-	// 		id BINARY(16) PRIMARY KEY,
-	// 		name VARCHAR(100) UNIQUE NOT NULL
-	// 	);
-	// 	"#
-	// )
-	// .execute(&mut *transaction)
-	// .await?;
+	log::info!("Initializing application tables");
+	query!(
+		r#"
+		CREATE TABLE IF NOT EXISTS application (
+			id BINARY(16) PRIMARY KEY,
+			name VARCHAR(100) UNIQUE NOT NULL
+		);
+		"#
+	)
+	.execute(&mut *transaction)
+	.await?;
 
-	// query!(
-	// 	r#"
-	// 	CREATE TABLE IF NOT EXISTS application_version (
-	// 		application_id BINARY(16) NOT NULL,
-	// 		version VARCHAR(32) NOT NULL,
-	// 		PRIMARY KEY(application_id, version),
-	// 		FOREIGN KEY(application_id) REFERENCES application(id)
-	// 	);
-	// 	"#
-	// )
-	// .execute(&mut *transaction)
-	// .await?;
+	query!(
+		r#"
+		CREATE TABLE IF NOT EXISTS application_version (
+			application_id BINARY(16) NOT NULL,
+			version VARCHAR(32) NOT NULL,
+			PRIMARY KEY(application_id, version),
+			FOREIGN KEY(application_id) REFERENCES application(id)
+		);
+		"#
+	)
+	.execute(&mut *transaction)
+	.await?;
 
-	// query!(
-	// 	r#"
-	// 	CREATE TABLE IF NOT EXISTS application_version_platform (
-	// 		application_id BINARY(16) NOT NULL,
-	// 		version VARCHAR(32) NOT NULL,
-	// 		platform VARCHAR(60) NOT NULL,
-	// 		PRIMARY KEY(application_id, version, platform),
-	// 		FOREIGN KEY(application_id, version)
-	// 			REFERENCES application_version(application_id, version)
-	// 	);
-	// 	"#
-	// )
-	// .execute(&mut *transaction)
-	// .await?;
+	query!(
+		r#"
+		CREATE TABLE IF NOT EXISTS application_version_platform (
+			application_id BINARY(16) NOT NULL,
+			version VARCHAR(32) NOT NULL,
+			platform VARCHAR(60) NOT NULL,
+			PRIMARY KEY(application_id, version, platform),
+			FOREIGN KEY(application_id, version)
+				REFERENCES application_version(application_id, version)
+		);
+		"#
+	)
+	.execute(&mut *transaction)
+	.await?;
 
 	Ok(())
 }
@@ -59,15 +59,15 @@ pub async fn initialize_application_pre(
 pub async fn initialize_application_post(
 	transaction: &mut Transaction<'_, MySql>,
 ) -> Result<(), sqlx::Error> {
-	// query!(
-	// 	r#"
-	// 	ALTER TABLE application
-	// 	ADD CONSTRAINT
-	// 	FOREIGN KEY(id) REFERENCES resource(id);
-	// 	"#
-	// )
-	// .execute(&mut *transaction)
-	// .await?;
+	query!(
+		r#"
+		ALTER TABLE application
+		ADD CONSTRAINT
+		FOREIGN KEY(id) REFERENCES resource(id);
+		"#
+	)
+	.execute(&mut *transaction)
+	.await?;
 
 	Ok(())
 }
@@ -81,23 +81,23 @@ pub async fn get_applications_for_organisation(
 ) -> Result<Vec<Application>, sqlx::Error> {
 	// sql query to fetch application names.
     
-    let rows : Vec<Application> = Vec::new();
-    // let rows = query_as!(
-	// 	Application,
-	// 	r#"
-	// 		SELECT 
-	// 			application.id,
-	// 			application.name
-	// 		FROM
-	// 			application
-	// 		WHERE
-	// 			resource.owner_id = ? AND
-	// 			resource.id = domain.id;
-	// 	"#,
-	// 	organisation_id
-	// )
-	// .fetch_all(connection)
-	// .await?;
+    // let rows : Vec<Application> = Vec::new();
+    let rows = query_as!(
+		Application,
+		r#"
+			SELECT 
+				application.id,
+				application.name
+			FROM
+				application
+			WHERE
+				resource.owner_id = ? AND
+				resource.id = domain.id;
+		"#,
+		organisation_id
+	)
+	.fetch_all(connection)
+	.await?;
 
     Ok(rows)
 }
@@ -109,24 +109,24 @@ pub async fn get_application_by_id (
     connection : &mut Transaction<'_, MySql>,
     application_id : &[u8],
 ) -> Result<Option<Application>, sqlx::Error> {
-    // let rows = query_as!(
-    //     Application,
-    //     r#"
-    //     SELECT 
-    //         id,
-    //         name
-    //     FROM
-    //         application
-    //     WHERE 
-    //         id = ?
-    //     "#,
-    //     application_id
-    // )
-    // .fetch_all(connection)
-    // .await?;
+    let rows = query_as!(
+        Application,
+        r#"
+        SELECT 
+            id,
+            name
+        FROM
+            application
+        WHERE 
+            id = ?
+        "#,
+        application_id
+    )
+    .fetch_all(connection)
+    .await?;
 
-    // Ok(rows.into_iter().next())
-    Ok(None)
+    Ok(rows.into_iter().next())
+    // Ok(None)
 }
 
 // query to fetch versions for an application.
@@ -136,24 +136,21 @@ pub async fn get_versions_for_application(
     appliction_id : &[u8],
 ) -> Result<Option<Version>, sqlx::Error> {
     
-    // let versions = query_as!(
-    //     Version,
-    //     r#"
-    //     SELECT 
-    //         application_id,
-    //         version
-    //     FROM 
-    //         application_version
-    //     WHERE
-    //         application_id = ?
-    //     "#,
-    //     appliction_id
-    // )
-    // .fetch_all(connection)
-    // .await?;
+    let versions = query_as!(
+        Version,
+        r#"
+        SELECT 
+            application_id,
+            version
+        FROM 
+            application_version
+        WHERE
+            application_id = ?
+        "#,
+        appliction_id
+    )
+    .fetch_all(connection)
+    .await?;
 
-
-    // return this 
-    // Ok(versions.into_iter().next())
-    Ok(None)
+    Ok(versions.into_iter().next())
 }
