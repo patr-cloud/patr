5ed36762b286f3ebe1ad874dc2a0e4763cc12501
Rakshith Ravi <rakshith.ravi@gmx.com>
2021-03-19 15:33:12 +0530

    Added start and end constraints to validator regex

--- api/src/utils/validator.rs
+++ api/src/utils/validator.rs
@@ -4,17 +4,17 @@ use tokio::sync::RwLock;
 
 lazy_static! {
 	// Can only contain a-z, A-Z, 0-9, . and _. Cannot begin with a . (github rules, basically)
-	static ref USERNAME_REGEX: Regex = Regex::new("[a-zA-Z0-9_]+[a-zA-Z0-9_\\.]*").unwrap();
+	static ref USERNAME_REGEX: Regex = Regex::new("^[a-zA-Z0-9_]+[a-zA-Z0-9_\\.]*$").unwrap();
 	// Email regex: https://stackoverflow.com/a/201378
-	static ref EMAIL_REGEX: Regex = Regex::new("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])").unwrap();
+	static ref EMAIL_REGEX: Regex = Regex::new("^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])$").unwrap();
 	// Needs to have a '+' to start with, and be between 9-15 numbers after that
-	static ref PHONE_NUMBER_REGEX: Regex = Regex::new("\\+?[0-9]{9,15}$").unwrap();
+	static ref PHONE_NUMBER_REGEX: Regex = Regex::new("^\\+?[0-9]{9,15}$").unwrap();
 	// Needs to have at least 1 a-z, 1 A-Z, 1 0-9 and a special character
 	//static ref PASSWORD_REGEX: Regex = Regex::new("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8,}$").unwrap();
 	// Needs to begin with personal-organisation- and follow up with a 128 bit hex
-	static ref PERSONAL_ORGANISATION_NAME_REGEX: Regex = Regex::new("personal-organisation-[a-z0-9]{32}").unwrap();
+	static ref PERSONAL_ORGANISATION_NAME_REGEX: Regex = Regex::new("^personal-organisation-[a-z0-9]{32}$").unwrap();
 	// Can only contain lowercase letters, numbers, hyphens and underscores
-	static ref DOCKER_REPO_NAME_REGEX: Regex = Regex::new("[a-z0-9_-]{2,255}").unwrap();
+	static ref DOCKER_REPO_NAME_REGEX: Regex = Regex::new("^[a-z0-9_-]{2,255}$").unwrap();
 	// List of all TLDs supported by ICANN. Updated every week.
 	pub(crate) static ref DOMAIN_TLD_LIST: RwLock<Vec<String>> = RwLock::new(vec![]);
 }
