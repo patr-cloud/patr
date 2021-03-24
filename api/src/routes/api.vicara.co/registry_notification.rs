33ee27ec0663dc0f108151cb4d7599668a64c3aa
Manjeet Arneja <manjeet.arneja@vicara.co>
2021-03-24 13:33:04 +0000

    added basci code to handle incoming event notifications

--- api/src/routes/api.vicara.co/registry_notification.rs
+++ api/src/routes/api.vicara.co/registry_notification.rs
@@ -1,11 +1,12 @@
 use api_macros::closure_as_pinned_box;
 use eve_rs::{App as EveApp, Context, Error, NextHandler};
-use validator::is_docker_repo_name_valid;
+use futures::StreamExt;
+use shiplift::{Docker, Images, PullOptions};
 
 use crate::{
 	app::{create_eve_app, App},
 	db, error,
-	models::db_mapping::Event,
+	models::db_mapping::EventData,
 	models::rbac::{self, permissions},
 	pin_fn,
 	utils::{constants::request_keys, validator, EveContext, EveMiddleware},
@@ -23,12 +24,48 @@ pub fn create_sub_app(app: &App) -> EveApp<EveContext, EveMiddleware, App> {
 }
 
 pub async fn notification_handler(
-	context: EveContext,
+	mut context: EveContext,
 	_: NextHandler<EveContext>,
 ) -> Result<EveContext, Error<EveContext>> {
 	let body = context.get_body_object().clone();
-	let event: Event = serde_json::from_value(body)?;
+	let events: EventData = serde_json::from_value(body)?;
 
-	println!("Received request body is {:#?}", event);
+	// check if the event is a push event
+	// get image name, repository name, tag if present
+	for event in events.events {
+		if event.action == "push" {
+			let target = event.target;
+			let tag = target.tag;
+			if tag == "develop" {
+				let mut repository_name = target.repository;
+
+				// pull the image
+				let mut image_name = format!("{}/{}", &repository_name, &tag);
+				// repository_name.push_str(format!("/{}", &tag).as_str());
+
+				let docker = Docker::new();
+
+				// let image = docker.images().new(&docker);
+				let image = Images::new(&docker);
+				let pull_options =
+					PullOptions::builder().image(image_name).build();
+
+				// pull image
+				let mut something = image.pull(&pull_options);
+				while let Some(value) = something.next().await {
+					//TODO: handle error for this
+					if let Err(err) = value {
+						context.status(500);
+						return Ok(context);
+					}
+					let result = value.unwrap();
+					log::debug!("received info")
+				}
+				// log::info!("generated image object {:#?}", );
+				log::info!("DOING SOMETHING...");
+			}
+		}
+	}
+
 	Ok(context)
 }
