d5a15b5fb93bc58f54bac603699d9b2652af6b31
Manjeet Arneja <manjeet.arneja@vicara.co>
2021-03-24 07:26:07 +0000

    completed Event Struct

--- api/src/models/db_mapping/deployer.rs
+++ api/src/models/db_mapping/deployer.rs
@@ -11,45 +11,51 @@ pub struct DockerRepository {
 	pub name: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(rename_all = "camelCase")]
+#[serde(default)]
 pub struct Event {
 	pub id: String,
-	pub time_stamp: Duration,
+	pub time_stamp: String,
 	pub action: String,
 	pub target: Target,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(rename_all = "camelCase")]
-pub struct Target {
-	pub media_type: String,
-	pub size: i64,
-	pub digest: String,
-	pub length: u64,
-	pub urls: Vec<String>,
-	pub repository: String,
-	pub url: String,
-	pub tag: String,
 	pub request: Request,
 	pub actor: Actor,
 	pub source: Source,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(rename_all = "camelCase")]
+#[serde(default)]
+pub struct Target {
+	pub media_type: String,
+	pub size: i64,
+	pub digest: String,
+	pub length: u64,
+	pub repository: String,
+	pub url: String,
+	pub tag: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+#[serde(rename_all = "camelCase")]
+#[serde(default)]
 pub struct Request {
 	pub id: String,
 	pub addr: String,
 	pub host: String,
 	pub method: String,
-	pub user_agent: String,
+	pub useragent: String,
 }
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(rename_all = "camelCase")]
-pub struct Actor {}
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(default)]
+pub struct Actor {
+	pub name: String,
+}
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(rename_all = "camelCase")]
+#[serde(default)]
 pub struct Source {
 	pub addr: String,
 	pub instance_id: String,
--- api/src/routes/api.vicara.co/registry_notification.rs
+++ api/src/routes/api.vicara.co/registry_notification.rs
@@ -5,11 +5,12 @@ use validator::is_docker_repo_name_valid;
 use crate::{
 	app::{create_eve_app, App},
 	db, error,
+	models::db_mapping::Event,
 	models::rbac::{self, permissions},
 	pin_fn,
 	utils::{constants::request_keys, validator, EveContext, EveMiddleware},
 };
-use serde_json::{json, Value};
+use serde_json::{json, Deserializer, Value};
 
 pub fn create_sub_app(app: &App) -> EveApp<EveContext, EveMiddleware, App> {
 	let mut sub_app = create_eve_app(app);
@@ -25,5 +26,9 @@ pub async fn notification_handler(
 	context: EveContext,
 	_: NextHandler<EveContext>,
 ) -> Result<EveContext, Error<EveContext>> {
+	let body = context.get_body_object().clone();
+	let event: Event = serde_json::from_value(body)?;
+
+	println!("Received request body is {:#?}", event);
 	Ok(context)
 }
