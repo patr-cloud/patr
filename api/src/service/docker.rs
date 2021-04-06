6d1e2fbac93917f3531e67b3f493a113424dcfdf
Manjeet Arneja <manjeet.arneja@vicara.co>
2021-04-06 14:29:26 +0000

    updated docker delete function in service

--- api/src/service/docker.rs
+++ api/src/service/docker.rs
@@ -17,9 +17,29 @@ pub async fn stop_container(
 }
 
 /// delete container
+/// this function will take care of stopping a container if it is running and then deleting it.
 pub async fn delete_container(
 	docker: &Docker,
 	container_name: &str,
 ) -> Result<(), Error> {
-	docker.containers().get(&container_name).delete().await
+	// stop container
+	let container_stop_result = stop_container(docker, container_name).await;
+
+	let container_delete_result =
+		docker.containers().get(&container_name).delete().await;
+
+	// if container is not deleted, then start back the contaienr
+	if container_delete_result.is_err() {
+		let container_start_result =
+			docker.containers().get(&container_name).start().await;
+		if let Err(err) = container_start_result {
+			// throw error
+			return Err(err);
+		}
+	}
+
+	if let Err(err) = container_stop_result.and(container_delete_result) {
+		return Err(err);
+	}
+	Ok(())
 }
