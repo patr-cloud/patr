0b5a94ebb48f03b557589c255778fc3541f9566a
Manjeet Arneja <manjeet.arneja@vicara.co>
2021-05-18 21:20:31 +0000

    1) added a service function to fetch deployment config details. 2) created a DeploymentConfig struct in db mapping, 3) added db queries to fetch ports, variables and volumme mounts for the given deployment

--- api/src/db/organisation/deployment.rs
+++ api/src/db/organisation/deployment.rs
@@ -1,7 +1,9 @@
 use sqlx::{MySql, Transaction};
 
 use crate::{
-	models::db_mapping::{Deployment, DockerRepository, MachineType},
+	models::db_mapping::{
+		Deployment, DockerRepository, EnvVariable, MachineType, VolumeMount,
+	},
 	query, query_as,
 };
 
@@ -23,7 +25,7 @@ pub async fn initialize_deployer_pre(
 			/* TODO change port to port array, and take image from docker_registry_repository */
 			persistence BOOL NOT NULL,
 			datacenter VARCHAR(255) NOT NULL,
-			UNIQUE(domain_id, sub_domain, path, volume_id, var_id)
+			UNIQUE(domain_id, sub_domain, path, var_id)
 		);
 		"#
 	)
@@ -556,10 +558,64 @@ pub async fn insert_deployment_environment_variables(
 }
 
 // function to return list of ports
-// pub async fn get_ports_by_deployment_id(
-// 	connection: &mut Transaction<'_, MySql>,
-// 	deployment_id: &[u8],
-// ) -> Result<Vec<u8>, sqlx::Error> {
-// }
+pub async fn get_ports_for_deployment(
+	connection: &mut Transaction<'_, MySql>,
+	deployment_id: &[u8],
+) -> Result<Vec<u8>, sqlx::Error> {
+	Ok(query!(
+		r#"
+			SELECT
+				port
+			FROM
+				port
+			WHERE
+				deployment_id = ?;
+			"#,
+		deployment_id
+	)
+	.fetch_all(connection)
+	.await?
+	.into_iter()
+	.map(|val| val.port as u8)
+	.collect())
+}
 // function to return list of env variables
+pub async fn get_variables_for_deployment(
+	connection: &mut Transaction<'_, MySql>,
+	deployment_id: &[u8],
+) -> Result<Vec<EnvVariable>, sqlx::Error> {
+	query_as!(
+		EnvVariable,
+		r#"
+			SELECT
+				*
+			FROM
+				environment_variable
+			WHERE
+				deployment_id = ?;
+			"#,
+		deployment_id
+	)
+	.fetch_all(connection)
+	.await
+}
 // function to return list of volume mounts
+pub async fn get_volume_mounts_for_deployment(
+	connection: &mut Transaction<'_, MySql>,
+	deployment_id: &[u8],
+) -> Result<Vec<VolumeMount>, sqlx::Error> {
+	query_as!(
+		VolumeMount,
+		r#"
+			SELECT
+				*
+			FROM
+				volume
+			WHERE
+				deployment_id = ?;
+			"#,
+		deployment_id
+	)
+	.fetch_all(connection)
+	.await
+}
--- api/src/service/deployment.rs
+++ api/src/service/deployment.rs
@@ -8,6 +8,7 @@ use crate::{
 	utils::{validator, Error},
 };
 
+// function to return deployment config details
 pub async fn get_deployment_config_by_id(
 	connection: &mut Transaction<'_, MySql>,
 	deployment_id: &[u8],
@@ -18,10 +19,27 @@ pub async fn get_deployment_config_by_id(
 	if deployment.is_none() {
 		Error::as_result()
 			.status(400)
-			.body(error!(RESOURCE_DOES_NOT_EXISTS).to_string())?;
+			.body(error!(RESOURCE_DOES_NOT_EXIST).to_string())?;
 	}
+	let deployment = deployment.unwrap();
+	let port_list =
+		db::get_ports_for_deployment(connection, deployment_id).await?;
+	let variable_list =
+		db::get_variables_for_deployment(connection, deployment_id).await?;
+	let volume_mount_list =
+		db::get_volume_mounts_for_deployment(connection, deployment_id).await?;
 
-	// get deployment details from deployment table
-
-	Ok(())
+	Ok(Some(DeploymentConfig {
+		id: deployment.id,
+		name: deployment.name,
+		registry: deployment.registry,
+		image_name: deployment.image_name,
+		image_tag: deployment.image_tag,
+		domain_id: deployment.domain_id,
+		sub_domain: deployment.sub_domain,
+		path: deployment.path,
+		port_list,
+		env_variable_list: variable_list,
+		volume_mount_list,
+	}))
 }
