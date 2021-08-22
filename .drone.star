2155f35b6e5f05e6433c2817f054390a37b6a378
Rakshith Ravi <rakshith@vicara.co>
2021-08-22 21:35:27 +0530

    Removed specific IP addresses for develop.vicara.co

--- .drone.star
+++ .drone.star
@@ -366,9 +366,6 @@ def create_gitea_release(step_name, staging):
         "environment": {
             "GITEA_TOKEN": {
                 "from_secret": "gitea_token"
-            },
-            "GITEA_IP": {
-                "from_secret": "gitea_ip"
             }
         }
     }
--- api/examples/gitea-release.rs
+++ api/examples/gitea-release.rs
@@ -21,7 +21,6 @@ async fn main() {
 	let repo_name =
 		env::var("DRONE_REPO_NAME").expect("DRONE_REPO_NAME is not set");
 
-	let gitea_ip = env::var("GITEA_IP").expect("GITEA_IP is not set");
 	let gitea_token = env::var("GITEA_TOKEN").expect("GITEA_TOKEN not set");
 	let release_version = {
 		let version = Version::parse(crate_version)
@@ -35,20 +34,11 @@ async fn main() {
 		)
 	};
 
-	let client = reqwest::Client::builder()
-		.resolve(
-			&system_host,
-			format!("{}:443", gitea_ip)
-				.parse()
-				.expect("cannot convert IP address to SocketAddr"),
-		)
-		.build()
-		.expect("unable to build reqwest client");
+	let client = reqwest::Client::new();
 	let url = format!(
 		"{}://{}/api/v1/repos/{}/{}/releases",
 		system_proto, system_host, repo_owner, repo_name
 	);
-	println!("Posting to {}", url);
 	let response = client
 		.post(url)
 		.header("Authorization", format!("token {}", gitea_token))
