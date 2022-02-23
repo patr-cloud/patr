42b825e505adcc885fb12f1f5ed12e0d963ab10a
ashish.oli <ashish.oli@vicara.co>
2022-02-23 08:23:37 +0000

    fix: changed default db inserts to named inserts

--- api/src/db/workspace/infrastructure/deployment.rs
+++ api/src/db/workspace/infrastructure/deployment.rs
@@ -334,7 +334,12 @@ pub async fn initialize_deployment_post(
 		query!(
 			r#"
 			INSERT INTO
-				deployment_machine_type
+				deployment_machine_type 
+				(
+					id, 
+					cpu_count, 
+					memory_count
+				)
 			VALUES
 				($1, $2, $3);
 			"#,
@@ -379,7 +384,22 @@ pub async fn create_deployment_with_internal_registry(
 	query!(
 		r#"
 		INSERT INTO
-			deployment
+			deployment 
+			(
+				id, 
+				name, 
+				registry, 
+				repository_id, 
+				image_name, 
+				image_tag, 
+				status, 
+				workspace_id, 
+				region, 
+				min_horizontal_scale, 
+				max_horizontal_scale, 
+				machine_type,
+				deploy_on_push
+			)
 		VALUES
 			(
 				$1,
@@ -430,7 +450,22 @@ pub async fn create_deployment_with_external_registry(
 	query!(
 		r#"
 		INSERT INTO
-			deployment
+			deployment 
+			(
+				id, 
+				name,
+				registry, 
+				repository_id, 
+				image_name, 
+				image_tag, 
+				status, 
+				workspace_id, 
+				region, 
+				min_horizontal_scale, 
+				max_horizontal_scale, 
+				machine_type, 
+				deploy_on_push
+			)
 		VALUES
 			(
 				$1,
@@ -730,7 +765,12 @@ pub async fn add_environment_variable_for_deployment(
 	query!(
 		r#"
 		INSERT INTO 
-			deployment_environment_variable
+			deployment_environment_variable 
+			(
+				deployment_id, 
+				name, 
+				value
+			)
 		VALUES
 			($1, $2, $3, $4);
 		"#,
@@ -796,7 +836,12 @@ pub async fn add_exposed_port_for_deployment(
 	query!(
 		r#"
 		INSERT INTO 
-			deployment_exposed_port
+			deployment_exposed_port 
+			(
+				deployment_id, 
+				port, 
+				port_type
+			)
 		VALUES
 			($1, $2, $3);
 		"#,
@@ -992,7 +1037,14 @@ async fn populate_region(
 		query!(
 			r#"
 			INSERT INTO
-				deployment_region
+				deployment_region 
+				(
+					id, 
+					name, 
+					provider, 
+					location, 
+					parent_region_id
+				)
 			VALUES
 				($1, $2, $3, ST_SetSRID(POINT($4, $5)::GEOMETRY, 4326), $6);
 			"#,
@@ -1010,7 +1062,14 @@ async fn populate_region(
 		query!(
 			r#"
 			INSERT INTO
-				deployment_region
+				deployment_region 
+				(
+					id, 
+					name, 
+					provider, 
+					location, 
+					parent_region_id
+				)
 			VALUES
 				($1, $2, NULL, NULL, $3);
 			"#,
