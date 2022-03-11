c6716cfcef5a1a901ea638b0c4ae9d8b1a306ee3
Rakshith Ravi <rakshith.ravi@gmx.com>
2022-03-11 13:56:45 +0530

    Added migrations for 0.5.7. Migrated all static site ingresses to not use the secretName for TLS

--- api/src/migrations/from_v0/from_v0_5.rs
+++ api/src/migrations/from_v0/from_v0_5.rs
@@ -61,6 +61,7 @@ pub async fn migrate(
 		(0, 5, 3) => migrate_from_v0_5_3(&mut *connection, config).await?,
 		(0, 5, 4) => migrate_from_v0_5_4(&mut *connection, config).await?,
 		(0, 5, 5) => migrate_from_v0_5_5(&mut *connection, config).await?,
+		(0, 5, 6) => migrate_from_v0_5_6(&mut *connection, config).await?,
 		_ => {
 			panic!("Migration from version {} is not implemented yet!", version)
 		}
@@ -77,7 +78,9 @@ pub async fn migrate(
 /// This function returns [&'static str; _] containing a list of all migration
 /// versions
 pub fn get_migrations() -> Vec<&'static str> {
-	vec!["0.5.0", "0.5.1", "0.5.2", "0.5.3", "0.5.4", "0.5.5"]
+	vec![
+		"0.5.0", "0.5.1", "0.5.2", "0.5.3", "0.5.4", "0.5.5", "0.5.6",
+	]
 }
 
 async fn migrate_from_v0_5_0(
@@ -566,6 +569,159 @@ async fn migrate_from_v0_5_5(
 		.patch(
 			&format!("ingress-{}", deployment_id),
 			&PatchParams::apply(&format!("ingress-{}", deployment_id)),
+			&Patch::Apply(kubernetes_ingress),
+		)
+		.await
+		.map_err(|err| sqlx::Error::Configuration(Box::new(err)))?;
+	}
+
+	Ok(())
+}
+
+async fn migrate_from_v0_5_6(
+	connection: &mut <Database as sqlx::Database>::Connection,
+	config: &Settings,
+) -> Result<(), sqlx::Error> {
+	let workspaces = query!(
+		r#"
+		SELECT
+			id
+		FROM
+			workspace;
+		"#
+	)
+	.fetch_all(&mut *connection)
+	.await?;
+
+	if workspaces.is_empty() {
+		return Ok(());
+	}
+
+	let static_site_list = query!(
+		r#"
+		SELECT
+			id,
+			workspace_id
+		FROM
+			deployment_static_site
+		WHERE
+			status = 'running';
+		"#
+	)
+	.fetch_all(&mut *connection)
+	.await?
+	.into_iter()
+	.map(|row| (row.get::<Uuid, _>("id"), row.get::<Uuid, _>("workspace_id")));
+
+	let kubernetes_config = Config::from_custom_kubeconfig(
+		Kubeconfig {
+			preferences: None,
+			clusters: vec![NamedCluster {
+				name: config.kubernetes.cluster_name.clone(),
+				cluster: Cluster {
+					server: config.kubernetes.cluster_url.clone(),
+					insecure_skip_tls_verify: None,
+					certificate_authority: None,
+					certificate_authority_data: Some(
+						config.kubernetes.certificate_authority_data.clone(),
+					),
+					proxy_url: None,
+					extensions: None,
+				},
+			}],
+			auth_infos: vec![NamedAuthInfo {
+				name: config.kubernetes.auth_name.clone(),
+				auth_info: AuthInfo {
+					username: Some(config.kubernetes.auth_username.clone()),
+					token: Some(config.kubernetes.auth_token.clone()),
+					..Default::default()
+				},
+			}],
+			contexts: vec![NamedContext {
+				name: config.kubernetes.context_name.clone(),
+				context: Context {
+					cluster: config.kubernetes.cluster_name.clone(),
+					user: config.kubernetes.auth_username.clone(),
+					extensions: None,
+					namespace: None,
+				},
+			}],
+			current_context: Some(config.kubernetes.context_name.clone()),
+			extensions: None,
+			kind: Some("Config".to_string()),
+			api_version: Some("v1".to_string()),
+		},
+		&Default::default(),
+	)
+	.await
+	.map_err(|err| sqlx::Error::Configuration(Box::new(err)))?;
+
+	let kubernetes_client = kube::Client::try_from(kubernetes_config)
+		.map_err(|err| sqlx::Error::Configuration(Box::new(err)))?;
+
+	let ingress_tls_rules = IngressTLS {
+		hosts: Some(vec!["*.patr.cloud".to_string(), "patr.cloud".to_string()]),
+		secret_name: None,
+	};
+
+	for (static_site_id, workspace_id) in static_site_list {
+		let annotations = [
+			(
+				"kubernetes.io/ingress.class".to_string(),
+				"nginx".to_string(),
+			),
+			(
+				"nginx.ingress.kubernetes.io/upstream-vhost".to_string(),
+				format!("{}.patr.cloud", static_site_id),
+			),
+			(
+				"cert-manager.io/cluster-issuer".to_string(),
+				config.kubernetes.cert_issuer_dns.clone(),
+			),
+		]
+		.into_iter()
+		.collect::<BTreeMap<_, _>>();
+
+		let kubernetes_ingress = Ingress {
+			metadata: ObjectMeta {
+				name: Some(format!("ingress-{}", static_site_id)),
+				annotations: Some(annotations),
+				..ObjectMeta::default()
+			},
+			spec: Some(IngressSpec {
+				rules: Some(vec![IngressRule {
+					host: Some(format!("{}.patr.cloud", static_site_id)),
+					http: Some(HTTPIngressRuleValue {
+						paths: vec![HTTPIngressPath {
+							backend: IngressBackend {
+								service: Some(IngressServiceBackend {
+									name: format!("service-{}", static_site_id),
+									port: Some(ServiceBackendPort {
+										number: Some(80),
+										..ServiceBackendPort::default()
+									}),
+								}),
+								..Default::default()
+							},
+							path: Some("/".to_string()),
+							path_type: Some("Prefix".to_string()),
+						}],
+					}),
+				}]),
+				tls: Some(vec![ingress_tls_rules.clone()]),
+				..IngressSpec::default()
+			}),
+			..Ingress::default()
+		};
+
+		// Create the ingress defined above
+		Api::<Ingress>::namespaced(
+			kubernetes_client.clone(),
+			workspace_id.as_str(),
+		)
+		.patch(
+			&format!("ingress-{}", static_site_id),
+			&PatchParams::apply(&format!("ingress-{}", static_site_id)),
 			&Patch::Apply(kubernetes_ingress),
 		)
 		.await
