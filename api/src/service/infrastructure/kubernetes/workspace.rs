670b1e607d072140e7ff54f19d84b52d2913a734
ashish.oli <ashish.oli@vicara.co>
2022-03-24 11:00:54 +0000

    added: function for deleting kubernetes namespace

--- api/src/service/infrastructure/kubernetes/workspace.rs
+++ api/src/service/infrastructure/kubernetes/workspace.rs
@@ -1,6 +1,11 @@
 use api_models::utils::Uuid;
 use k8s_openapi::{self, api::core::v1::Namespace};
-use kube::{self, api::PostParams, core::ObjectMeta, Api};
+use kube::{
+	self,
+	api::{DeleteParams, PostParams},
+	core::ObjectMeta,
+	Api,
+};
 
 use crate::utils::{settings::Settings, Error};
 
@@ -24,6 +29,24 @@ pub async fn create_kubernetes_namespace(
 		.create(&PostParams::default(), &kubernetes_namespace)
 		.await?;
 	log::trace!("request_id: {} - namespace created", request_id);
+
+	Ok(())
+}
+
+pub async fn delete_kubernetes_namespace(
+	namespace_name: &str,
+	config: &Settings,
+	request_id: &Uuid,
+) -> Result<(), Error> {
+	let client = super::get_kubernetes_config(config).await?;
+
+	log::trace!("request_id: {} - creating namespace", request_id);
+
+	let namespace_api = Api::<Namespace>::all(client.clone());
+	namespace_api
+		.delete(namespace_name, &DeleteParams::default())
+		.await?;
+	log::trace!("request_id: {} - deleted namespace", request_id);
 
 	Ok(())
 }
