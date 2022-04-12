71cb2853172f1814534892561fd76a53b156ecb1
ashish.oli <ashish.oli@vicara.co>
2022-04-12 07:42:53 +0000

    fix: constraints for one of two column in deployment_env_var table should have a value

--- api/src/migrations/from_v0/from_v0_6/from_v0_6_2.rs
+++ api/src/migrations/from_v0/from_v0_6/from_v0_6_2.rs
@@ -406,8 +406,7 @@ async fn add_secret_id_column_to_deployment_environment_variable(
 		r#"
 		ALTER TABLE deployment_environment_variable
 			ADD CONSTRAINT deployment_environment_variable_chk_value_secret_id_both_not_null
-				CHECK((value IS NULL OR secret_id IS NULL) 
-				AND NOT (value IS NULL AND secret_id IS NULL));
+				CHECK(value IS NOT NULL OR secret_id IS NOT NULL);
 		"#
 	)
 	.execute(&mut *connection)
