f2c1ae0534d3eeffaeb835cced78b3457f2a8c3f
ashish.oli <ashish.oli@vicara.co>
2022-04-18 13:34:11 +0000

    fix: dockerfile for flask

--- api/src/service/github_actions_config.rs
+++ api/src/service/github_actions_config.rs
@@ -672,7 +672,7 @@ jobs:
               WORKDIR /app
               COPY . .
               RUN pip3 install -r requirements.txt
-              CMD ["python3", "app.py"]
+              CMD ["python3", "src/app.py"]
               " > Dockerfile
 
         - name: Docker login
