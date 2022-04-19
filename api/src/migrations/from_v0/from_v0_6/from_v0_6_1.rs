dc0412d6aa8511629f727a95d86f24b22789254a
Rakshith Ravi <rakshith.ravi@gmx.com>
2022-04-19 22:16:23 +0530

    Made ManagedUrl ports constraint deferrable

--- api/src/migrations/from_v0/from_v0_6/from_v0_6_1.rs
+++ api/src/migrations/from_v0/from_v0_6/from_v0_6_1.rs
@@ -1,8 +1,18 @@
-use crate::{utils::settings::Settings, Database};
+use crate::{utils::settings::Settings, Database, migrate_query as query};
 
 pub async fn migrate(
-	_connection: &mut <Database as sqlx::Database>::Connection,
+	connection: &mut <Database as sqlx::Database>::Connection,
 	_config: &Settings,
 ) -> Result<(), sqlx::Error> {
+	query!(
+		r#"
+		ALTER TABLE managed_url
+		ALTER CONSTRAINT managed_url_fk_deployment_id_port
+		SET DEFERRABLE INITIALLY IMMEDIATE;
+		"#
+	)
+	.execute(&mut *connection)
+	.await?;
+
 	Ok(())
 }
