04fe4311b7382b727cfba85c6487256b3fc00a2d
ashish.oli <ashish.oli@vicara.co>
2022-05-22 11:35:09 +0000

    added: activate github repo route

--- api/src/routes/api.patr.cloud/workspace/ci/github.rs
+++ api/src/routes/api.patr.cloud/workspace/ci/github.rs
@@ -2,13 +2,17 @@ use api_macros::closure_as_pinned_box;
 use api_models::{
 	models::workspace::ci::github::{
 		GithubAuthCallbackRequest,
+		ActivateGithubRepoRequest,
+		ActivateGithubRepoResponse,
 		GithubAuthCallbackResponse,
 		GithubAuthResponse,
+		GithubListRepos,
+		GithubListReposResponse,
 	},
 	utils::Uuid,
 };
 use eve_rs::{App as EveApp, AsError, Context, NextHandler};
-use reqwest::header::COOKIE;
+use reqwest::header::{AUTHORIZATION, COOKIE};
 
 use crate::{
 	app::{create_eve_app, App},
@@ -106,6 +110,99 @@ pub fn create_sub_app(
 		],
 	);
 
+	app.get(
+		"/list-repositories",
+		[
+			EveMiddleware::ResourceTokenAuthenticator(
+				permissions::workspace::github::auth::CREATE,
+				closure_as_pinned_box!(|mut context| {
+					let workspace_id =
+						context.get_param(request_keys::WORKSPACE_ID).unwrap();
+					let workspace_id = Uuid::parse_str(workspace_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let resource = db::get_resource_by_id(
+						context.get_database_connection(),
+						&workspace_id,
+					)
+					.await?;
+
+					if resource.is_none() {
+						context
+							.status(404)
+							.json(error!(RESOURCE_DOES_NOT_EXIST));
+					}
+
+					Ok((context, resource))
+				}),
+			),
+			EveMiddleware::CustomFunction(pin_fn!(list_repositories)),
+		],
+	);
+
+	app.get(
+		"/repo/activate",
+		[
+			EveMiddleware::ResourceTokenAuthenticator(
+				permissions::workspace::github::auth::CREATE,
+				closure_as_pinned_box!(|mut context| {
+					let workspace_id =
+						context.get_param(request_keys::WORKSPACE_ID).unwrap();
+					let workspace_id = Uuid::parse_str(workspace_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let resource = db::get_resource_by_id(
+						context.get_database_connection(),
+						&workspace_id,
+					)
+					.await?;
+
+					if resource.is_none() {
+						context
+							.status(404)
+							.json(error!(RESOURCE_DOES_NOT_EXIST));
+					}
+
+					Ok((context, resource))
+				}),
+			),
+			EveMiddleware::CustomFunction(pin_fn!(activate_repo)),
+		],
+	);
+
+	app.get(
+		"/repo/build-logs",
+		[
+			EveMiddleware::ResourceTokenAuthenticator(
+				permissions::workspace::github::auth::CREATE,
+				closure_as_pinned_box!(|mut context| {
+					let workspace_id =
+						context.get_param(request_keys::WORKSPACE_ID).unwrap();
+					let workspace_id = Uuid::parse_str(workspace_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let resource = db::get_resource_by_id(
+						context.get_database_connection(),
+						&workspace_id,
+					)
+					.await?;
+
+					if resource.is_none() {
+						context
+							.status(404)
+							.json(error!(RESOURCE_DOES_NOT_EXIST));
+					}
+
+					Ok((context, resource))
+				}),
+			),
+			EveMiddleware::CustomFunction(pin_fn!(get_build_logs)),
+		],
+	);
+
 	app
 }
 
@@ -161,5 +258,115 @@ async fn github_oauth_callback(
 	}
 
 	context.success(GithubAuthCallbackResponse {});
+	Ok(context)
+}
+
+async fn list_repositories(
+	mut context: EveContext,
+	_: NextHandler<EveContext, ErrorData>,
+) -> Result<EveContext, Error> {
+	let config = context.get_state().config.clone();
+
+	let user_hash = context
+	.get_request()
+	.get_query()
+	.get(request_keys::TOKEN)
+	.status(400)
+	.body(error!(WRONG_PARAMETERS).to_string())?;
+
+	// let user_hash = "KSWLvpAgXXExejCdYF8KVjBbIzG8llMr";
+
+	let client = reqwest::Client::new();
+	let response = client
+		.get(format!("{}/api/user/repos", config.drone.url))
+		.header(AUTHORIZATION, format!("Bearer {}", user_hash))
+		.send()
+		.await?;
+
+	if response.status() != 200 {
+		return Error::as_result()
+			.status(500)
+			.body(error!(SERVER_ERROR).to_string());
+	}
+
+	let repos = response.json::<Vec<GithubListRepos>>().await?;
+
+	context.success(GithubListReposResponse { repos });
+
+	Ok(context)
+}
+
+async fn activate_repo(
+	mut context: EveContext,
+	_: NextHandler<EveContext, ErrorData>,
+) -> Result<EveContext, Error> {
+	let config = context.get_state().config.clone();
+
+	let user_hash = context
+	.get_request()
+	.get_query()
+	.get(request_keys::TOKEN)
+	.status(400)
+	.body(error!(WRONG_PARAMETERS).to_string())?;
+
+	let ActivateGithubRepoRequest { owner, name, .. } = context
+		.get_body_as()
+		.status(400)
+		.body(error!(WRONG_PARAMETERS).to_string())?;
+
+	// let user_hash = "KSWLvpAgXXExejCdYF8KVjBbIzG8llMr";
+
+	let client = reqwest::Client::new();
+	let response = client
+		.post(format!("{}/api/repos/{}/{}", config.drone.url, owner, name))
+		.header(AUTHORIZATION, format!("Bearer {}", user_hash))
+		.send()
+		.await?;
+
+	if response.status() != 200 {
+		return Error::as_result()
+			.status(500)
+			.body(error!(SERVER_ERROR).to_string());
+	}
+
+	let activated_repo = response.json::<ActivateGithubRepoResponse>().await?;
+
+	context.success(activated_repo);
+
+	Ok(context)
+}
+
+async fn get_build_logs(
+	mut context: EveContext,
+	_: NextHandler<EveContext, ErrorData>,
+) -> Result<EveContext, Error> {
+	let config = context.get_state().config.clone();
+
+	let user_hash = context
+	.get_request()
+	.get_query()
+	.get(request_keys::TOKEN)
+	.status(400)
+	.body(error!(WRONG_PARAMETERS).to_string())?;
+
+	// let user_hash = "KSWLvpAgXXExejCdYF8KVjBbIzG8llMr";
+
+	let client = reqwest::Client::new();
+	let response = client
+		.get(format!("{}/api/user/repos", config.drone.url))
+		.header(AUTHORIZATION, format!("Bearer {}", user_hash))
+		.send()
+		.await?;
+
+	if response.status() != 200 {
+		return Error::as_result()
+			.status(500)
+			.body(error!(SERVER_ERROR).to_string());
+	}
+
+	let repos = response.json::<Vec<GithubListRepos>>().await?;
+
+	context.success(GithubListReposResponse { repos });
+
 	Ok(context)
 }
