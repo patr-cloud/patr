9a2534e479904c17aabf28842d7fc60ef4db3673
ashish.oli <ashish.oli@vicara.co>
2022-07-04 14:30:01 +0000

    feature: login with github, todo testing

--- api/src/db/user.rs
+++ api/src/db/user.rs
@@ -168,6 +168,7 @@ pub async fn initialize_users_pre(
 				coupon_code(code),
 			is_oauth_user BOOLEAN DEFAULT false,
 			oauth_access_token TEXT,
+
 			CONSTRAINT user_uq_recovery_email_local_recovery_email_domain_id
 				UNIQUE(recovery_email_local, recovery_email_domain_id),
 
@@ -2542,7 +2543,6 @@ pub async fn update_user_oauth_info(
 	connection: &mut <Database as sqlx::Database>::Connection,
 	access_token: &str,
 	user_id: &Uuid,
-	is_oauth_user: bool,
 ) -> Result<(), sqlx::Error> {
 	query!(
 		r#"
--- api/src/routes/api.patr.cloud/mod.rs
+++ api/src/routes/api.patr.cloud/mod.rs
@@ -14,6 +14,7 @@ use crate::{
 };
 
 mod auth;
+mod oauth;
 mod user;
 mod webhook;
 mod workspace;
@@ -40,6 +41,7 @@ pub fn create_sub_app(
 	let mut sub_app = create_eve_app(app);
 
 	sub_app.use_sub_app("/auth", auth::create_sub_app(app));
+	sub_app.use_sub_app("/oauth", oauth::create_sub_app(app));
 	sub_app.use_sub_app("/user", user::create_sub_app(app));
 	sub_app.use_sub_app("/workspace", workspace::create_sub_app(app));
 	sub_app.use_sub_app("/webhook", webhook::create_sub_app(app));
