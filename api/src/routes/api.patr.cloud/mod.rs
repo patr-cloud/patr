b1e64d1d849db598fda537eb37958844eaa625d3
ashish.oli <ashish.oli@vicara.co>
2022-07-06 07:46:40 +0000

    refactored code for auth and oauth functions and changed the file structure

--- api/src/routes/api.patr.cloud/mod.rs
+++ api/src/routes/api.patr.cloud/mod.rs
@@ -14,7 +14,6 @@ use crate::{
 };
 
 mod auth;
-mod oauth;
 mod user;
 mod webhook;
 mod workspace;
@@ -41,7 +40,6 @@ pub fn create_sub_app(
 	let mut sub_app = create_eve_app(app);
 
 	sub_app.use_sub_app("/auth", auth::create_sub_app(app));
-	sub_app.use_sub_app("/oauth", oauth::create_sub_app(app));
 	sub_app.use_sub_app("/user", user::create_sub_app(app));
 	sub_app.use_sub_app("/workspace", workspace::create_sub_app(app));
 	sub_app.use_sub_app("/webhook", webhook::create_sub_app(app));
