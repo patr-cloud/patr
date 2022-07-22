c2d31e5fbfa81df5ce32498d17394fa7417b71d9
kavin <kavin.raj@vicara.co>
2022-07-22 10:48:38 +0000

    Delete orphan resources in k8s

--- api/src/service/infrastructure/kubernetes/mod.rs
+++ api/src/service/infrastructure/kubernetes/mod.rs
@@ -191,3 +191,50 @@ pub async fn secret_exists(
 		Ok(_) => Ok(true),
 	}
 }
+
+pub mod ext_traits {
+	use std::fmt::Debug;
+
+	use async_trait::async_trait;
+	use either::Either;
+	use kube::{
+		api::DeleteParams,
+		client::Status,
+		core::ErrorResponse,
+		Api,
+		Error,
+		Result,
+	};
+	use serde::de::DeserializeOwned;
+
+	#[async_trait]
+	pub trait DeleteOpt<T> {
+		async fn delete_opt(
+			&self,
+			name: &str,
+			dp: &DeleteParams,
+		) -> Result<Option<Either<T, Status>>>;
+	}
+
+	#[async_trait]
+	impl<T> DeleteOpt<T> for Api<T>
+	where
+		T: Clone + DeserializeOwned + Debug,
+	{
+		async fn delete_opt(
+			&self,
+			name: &str,
+			dp: &DeleteParams,
+		) -> Result<Option<Either<T, Status>>> {
+			match self.delete(name, dp).await {
+				Ok(obj) => Ok(Some(obj)),
+				Err(Error::Api(ErrorResponse { reason, .. }))
+					if &reason == "NotFound" =>
+				{
+					Ok(None)
+				}
+				Err(err) => Err(err),
+			}
+		}
+	}
+}
--- api/src/syncr/mod.rs
+++ api/src/syncr/mod.rs
@@ -1,8 +1,20 @@
+use std::collections::HashSet;
+
 use api_models::utils::Uuid;
+use k8s_openapi::api::{
+	apps::v1::Deployment,
+	autoscaling::v1::HorizontalPodAutoscaler,
+	core::v1::Service,
+	networking::v1::Ingress,
+};
+use kube::{
+	api::{DeleteParams, PropagationPolicy},
+	Api,
+};
 
 use crate::{
 	db,
-	service,
+	service::{self, ext_traits::DeleteOpt},
 	utils::{settings::Settings, Error},
 	Database,
 };
@@ -17,11 +29,17 @@ pub async fn sync_deployments_in_workspace(
 	log::trace!("request_id: {request_id} - Syncing the deployments for workspace {workspace_id} from db to k8s");
 	let namespace = workspace_id.as_str();
 
+	log::trace!(
+		"request_id: {request_id} - Checking to create missing resources"
+	);
 	let running_deployments =
 		db::get_running_deployment_ids_for_workspace(connection, workspace_id)
 			.await?;
-
 	for deployment_id in &running_deployments {
+		log::trace!(
+			"request_id: {request_id} - Syncing deployment {deployment_id} "
+		);
+
 		let (
 			deployment,
 			_workspace_id,
@@ -35,9 +53,6 @@ pub async fn sync_deployments_in_workspace(
 		.await?;
 
 		// check deployment status
-		log::trace!(
-			"request_id: {request_id} - Sync: deployment {deployment_id} - syncing k8s deployment"
-		);
 		if !service::deployment_exists(
 			deployment_id,
 			kube_client.clone(),
@@ -73,7 +88,6 @@ pub async fn sync_deployments_in_workspace(
 		}
 
 		// check service status
-		log::trace!("request_id: {request_id} - Sync: deployment {deployment_id} - syncing k8s service");
 		if !service::service_exists(
 			deployment_id,
 			kube_client.clone(),
@@ -95,7 +109,6 @@ pub async fn sync_deployments_in_workspace(
 		}
 
 		// check pod scaler status
-		log::trace!("request_id: {request_id} - Sync: deployment {deployment_id} - syncing k8s hpa");
 		if !service::hpa_exists(deployment_id, kube_client.clone(), namespace)
 			.await?
 		{
@@ -111,7 +124,6 @@ pub async fn sync_deployments_in_workspace(
 		}
 
 		// check ingress status
-		log::trace!("request_id: {request_id} - Sync: deployment {deployment_id} - syncing k8s ingress");
 		if !service::ingress_exists(
 			deployment_id,
 			kube_client.clone(),
@@ -131,6 +143,137 @@ pub async fn sync_deployments_in_workspace(
 				config,
 			)
 			.await?;
+		}
+	}
+
+	log::trace!(
+		"request_id: {request_id} - Checking to create missing resources"
+	);
+	let all_deployments = db::get_all_deployment_ids_for_workspace(
+		&mut *connection,
+		workspace_id,
+	)
+	.await?
+	.into_iter()
+	.collect::<HashSet<_>>();
+
+	// TODO: make sure deployments, services, hpa & ingress
+	// are used only for deployments alone and not by other
+	// resources like static sites
+
+	// delete excess k8s deployments
+	let deployment_api = Api::<Deployment>::namespaced(
+		kube_client.clone(),
+		workspace_id.as_str(),
+	);
+	for deployment in deployment_api.list(&Default::default()).await? {
+		// deployment name won't be none
+		if let Some(deployment_name) = deployment.metadata.name {
+			let is_known = deployment_name
+				.strip_prefix("deployment-")
+				.and_then(|id_str| Uuid::parse_str(id_str).ok())
+				.map_or(false, |id| all_deployments.contains(&id));
+
+			if !is_known {
+				log::info!("request_id: {request_id} - Deleting unknown deployment `{deployment_name}` from k8s");
+				deployment_api
+					.delete_opt(
+						&deployment_name,
+						&DeleteParams {
+							propagation_policy: Some(
+								PropagationPolicy::Foreground,
+							),
+							..Default::default()
+						},
+					)
+					.await?;
+			}
+		}
+	}
+
+	// delete excess k8s services
+	let service_api =
+		Api::<Service>::namespaced(kube_client.clone(), workspace_id.as_str());
+	for service in service_api.list(&Default::default()).await? {
+		// service name won't be none
+		if let Some(service_name) = service.metadata.name {
+			let is_known = service_name
+				.strip_prefix("service-")
+				.and_then(|id_str| Uuid::parse_str(id_str).ok())
+				.map_or(false, |id| all_deployments.contains(&id));
+
+			if !is_known {
+				log::info!("request_id: {request_id} - Deleting unknown service `{service_name}` from k8s");
+				service_api
+					.delete_opt(
+						&service_name,
+						&DeleteParams {
+							propagation_policy: Some(
+								PropagationPolicy::Foreground,
+							),
+							..Default::default()
+						},
+					)
+					.await?;
+			}
+		}
+	}
+
+	// delete excess k8s hpa
+	let hpa_api = Api::<HorizontalPodAutoscaler>::namespaced(
+		kube_client.clone(),
+		workspace_id.as_str(),
+	);
+	for hpa in hpa_api.list(&Default::default()).await? {
+		// hpa name won't be none
+		if let Some(hpa_name) = hpa.metadata.name {
+			let is_known = hpa_name
+				.strip_prefix("hpa-")
+				.and_then(|id_str| Uuid::parse_str(id_str).ok())
+				.map_or(false, |id| all_deployments.contains(&id));
+
+			if !is_known {
+				log::info!("request_id: {request_id} - Deleting unknown hpa `{hpa_name}` from k8s");
+				hpa_api
+					.delete_opt(
+						&hpa_name,
+						&DeleteParams {
+							propagation_policy: Some(
+								PropagationPolicy::Foreground,
+							),
+							..Default::default()
+						},
+					)
+					.await?;
+			}
+		}
+	}
+
+	// delete excess k8s ingress
+	let ingress_api =
+		Api::<Ingress>::namespaced(kube_client.clone(), workspace_id.as_str());
+	for ingress in ingress_api.list(&Default::default()).await? {
+		// ingress name won't be none
+		if let Some(ingress_name) = ingress.metadata.name {
+			let is_known = ingress_name
+				.strip_prefix("ingress-")
+				.and_then(|id_str| Uuid::parse_str(id_str).ok())
+				.map_or(false, |id| all_deployments.contains(&id));
+
+			if !is_known {
+				log::info!("request_id: {request_id} - Deleting unknown ingress `{ingress_name}` from k8s");
+				ingress_api
+					.delete_opt(
+						&ingress_name,
+						&DeleteParams {
+							propagation_policy: Some(
+								PropagationPolicy::Foreground,
+							),
+							..Default::default()
+						},
+					)
+					.await?;
+			}
 		}
 	}
 
