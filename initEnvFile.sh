dad381a7b5900c978af10d1f253b0150ce9ba8fb
kavin <kavin.raj@vicara.co>
2022-09-11 16:30:15 +0000

    added todos

--- initEnvFile.sh
+++ initEnvFile.sh
@@ -16,7 +16,9 @@ fi
 
 # populate env file contents
 cat "./.env.template" | while read line; do
-    
+    # TODO: writing to the env file is not working as expected as
+    # the eval command is not escaping the value content when writing it to .env file
+
     printf "%q\n" $(eval echo \"$line\")
     # eval echo $line >> "$baseDir/.env"
 done
