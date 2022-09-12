452e5c4681e3d70460fb0c8305f7761c7010a53a
ashish.oli <ashish.oli@vicara.co>
2022-09-12 09:10:35 +0000

    fix: PR suggested changes for upding managed url on ss update

--- api/src/routes/api.patr.cloud/workspace/infrastructure/static_site.rs
+++ api/src/routes/api.patr.cloud/workspace/infrastructure/static_site.rs
@@ -762,6 +762,33 @@ async fn revert_static_site(
 	)
 	.await?;
 
+	context.commit_database_transaction().await?;
+
+	let managed_urls = db::get_managed_url_for_static_siite(
+		context.get_database_connection(),
+		&static_site_id,
+	)
+	.await?;
+
+	for managed_url in managed_urls {
+		service::update_kubernetes_managed_url(
+			&workspace_id,
+			&ManagedUrl {
+				id: managed_url.id,
+				sub_domain: managed_url.sub_domain,
+				domain_id: managed_url.domain_id,
+				path: managed_url.path,
+				url_type: ManagedUrlType::ProxyStaticSite {
+					static_site_id: static_site_id.to_owned(),
+				},
+				is_configured: managed_url.is_configured,
+			},
+			&config,
+			&request_id,
+		)
+		.await?;
+	}
+
 	context.success(RevertStaticSiteResponse {});
 	Ok(context)
 }
