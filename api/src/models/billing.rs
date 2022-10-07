6fce33f726cf25e3b43e63c061cf8fe2e197958d
ashish.oli <ashish.oli@vicara.co>
2022-10-07 07:39:39 +0000

    feature: invoice email with total resource usage breakdown

--- api/src/models/billing.rs
+++ api/src/models/billing.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
+
 use api_models::{
 	models::workspace::infrastructure::list_all_deployment_machine_type::DeploymentMachineType,
 	utils::Uuid,
 };
 use serde::{Deserialize, Serialize};
+
+use crate::db::{DomainPlan, StaticSitePlan};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -66,4 +70,17 @@ pub struct SecretsBill {
 	pub secrets_count: u64,
 	pub hours: u64,
 	pub amount: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ResourceUsageBill {
+	pub total_cost: f64,
+	pub deployment_usages: HashMap<Uuid, DeploymentBill>,
+	pub database_usages: HashMap<Uuid, DatabaseBill>,
+	pub static_sites_usages: HashMap<StaticSitePlan, StaticSiteBill>,
+	pub managed_url_usages: HashMap<u64, ManagedUrlBill>,
+	pub docker_repository_usages: Vec<DockerRepositoryBill>,
+	pub domains_usages: HashMap<DomainPlan, DomainBill>,
+	pub secrets_usages: HashMap<u64, SecretsBill>,
 }
