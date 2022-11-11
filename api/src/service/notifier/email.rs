1892f8c6696220d4b6f7f19858b26bb25acf5b85
kavin <kavin.raj@vicara.co>
2022-11-11 15:17:59 +0000

    Added testing for email templates

--- api/src/service/notifier/email.rs
+++ api/src/service/notifier/email.rs
@@ -1,4 +1,5 @@
 use api_models::models::workspace::billing::{Address, WorkspaceBillBreakdown};
+use handlebars::{handlebars_helper, Handlebars};
 use lettre::message::Mailbox;
 use serde::Serialize;
 
@@ -583,6 +584,31 @@ pub async fn send_domain_verified_email(
 	.await
 }
 
+fn get_configured_handlebar<'a>() -> Handlebars<'a> {
+	handlebars_helper!(cents_to_dollars: |cents: u64| format!("{:.2}", cents as f64 * 0.01));
+	handlebars_helper!(stringify_month: |month_in_num: u8| match month_in_num {
+		1 => "January",
+		2 => "February",
+		3 => "March",
+		4 => "April",
+		5 => "May",
+		6 => "June",
+		7 => "July",
+		8 => "August",
+		9 => "September",
+		10 => "October",
+		11 => "November",
+		12 => "December",
+		_ => "Invalid month"
+	});
+
+	let mut handlebar = Handlebars::new();
+	handlebar.register_helper("stringify-month", Box::new(stringify_month));
+	handlebar.register_helper("cents-to-dollars", Box::new(cents_to_dollars));
+
+	handlebar
+}
+
 /// # Description
 /// This function is used to send the email to a recipient
 ///
@@ -631,7 +657,8 @@ where
 			builder = builder.reply_to(reply_to);
 		}
 
-		let message = builder.multipart(body.render_body().await?)?;
+		let message = builder
+			.multipart(body.render_body(&get_configured_handlebar()).await?)?;
 
 		let credentials = Credentials::new(
 			settings.email.username.clone(),
@@ -668,7 +695,7 @@ where
 
 #[cfg(debug_assertions)]
 async fn send_email<TEmail>(
-	_body: TEmail,
+	body: TEmail,
 	to: Mailbox,
 	_reply_to: Option<Mailbox>,
 	_subject: &str,
@@ -677,5 +704,415 @@ where
 	TEmail: EmailTemplate,
 {
 	log::trace!("Sending email to {}", to);
+
+	let mut handlebar = get_configured_handlebar();
+	handlebar.set_strict_mode(true);
+
+	body.render_body(&handlebar).await.expect(
+		// safe to panic as it is will be used only in debug builds
+		"Handlebar template should be up-to-date with struct changes",
+	);
+
 	Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+	use std::collections::BTreeMap;
+
+	use api_models::{
+		models::workspace::{
+			billing::{
+				Address,
+				DatabaseUsage,
+				DeploymentBills,
+				DeploymentUsage,
+				DockerRepositoryUsage,
+				DomainPlan,
+				DomainUsage,
+				ManagedUrlUsage,
+				SecretUsage,
+				StaticSitePlan,
+				StaticSiteUsage,
+				WorkspaceBillBreakdown,
+			},
+			infrastructure::list_all_deployment_machine_type::DeploymentMachineType,
+		},
+		utils::Uuid,
+	};
+
+	use super::{
+		get_configured_handlebar,
+		AddEmailVerificationEmail,
+		BillNotPaidDeleteResourcesEmail,
+		BillPaidSuccessfullyEmail,
+		BillPaymentFailedReminderEmail,
+		CardNotAddedReminderEmail,
+		DomainUnverified,
+		DomainVerified,
+		ForgotPasswordEmail,
+		PasswordChangedEmail,
+		PasswordResetEmail,
+		PaymentFailureInvoiceEmail,
+		PaymentSuccessInvoiceEmail,
+		RecoveryNotificationEmail,
+		ResourceDeletedEmail,
+		SignUpCompletedEmail,
+		UserSignUpVerificationEmail,
+	};
+	use crate::{models::EmailTemplate, utils::Error};
+
+	async fn send_email<TEmail>(body: TEmail) -> Result<(), Error>
+	where
+		TEmail: EmailTemplate,
+	{
+		let mut handlebar = get_configured_handlebar();
+		handlebar.set_strict_mode(true);
+
+		body.render_body(&handlebar).await?;
+
+		Ok(())
+	}
+
+	#[tokio::test]
+	async fn test_user_sign_up_verification_email_email() -> Result<(), Error> {
+		send_email(UserSignUpVerificationEmail {
+			username: "username".to_owned(),
+			otp: "otp".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_forgot_password_email_email() -> Result<(), Error> {
+		send_email(ForgotPasswordEmail {
+			otp: "otp".to_owned(),
+			username: "username".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_password_reset_email_email() -> Result<(), Error> {
+		send_email(PasswordResetEmail {
+			username: "username".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_password_changed_email_email() -> Result<(), Error> {
+		send_email(PasswordChangedEmail {
+			username: "username".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_sign_up_completed_email_email() -> Result<(), Error> {
+		send_email(SignUpCompletedEmail {
+			username: "username".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_recovery_notification_email_email() -> Result<(), Error> {
+		send_email(RecoveryNotificationEmail {
+			username: "username".to_owned(),
+			recoveryEmail: "recoveryEmail".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_add_email_verification_email_email() -> Result<(), Error> {
+		send_email(AddEmailVerificationEmail {
+			otp: "otp".to_owned(),
+			username: "username".to_owned(),
+			recoveryEmail: "recoveryEmail".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_bill_not_paid_delete_resources_email_email(
+	) -> Result<(), Error> {
+		send_email(BillNotPaidDeleteResourcesEmail {
+			username: "username".to_owned(),
+			workspaceName: "workspaceName".to_owned(),
+			monthString: "month".to_owned(),
+			monthNumber: 4,
+			year: 2014,
+			totalBill: 567,
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_bill_payment_failed_reminder_email_email() -> Result<(), Error>
+	{
+		send_email(BillPaymentFailedReminderEmail {
+			username: "username".to_owned(),
+			workspaceName: "workspaceName".to_owned(),
+			monthString: "month".to_owned(),
+			monthNumber: 8,
+			year: 2351,
+			totalBill: 1234124,
+			deadline: "deadline".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_card_not_added_reminder_email_email() -> Result<(), Error> {
+		send_email(CardNotAddedReminderEmail {
+			username: "username".to_owned(),
+			workspaceName: "workspaceName".to_owned(),
+			monthString: "month".to_owned(),
+			monthNumber: 8,
+			year: 2351,
+			totalBill: 1234124,
+			deadline: "deadline".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_bill_paid_successfully_email_email() -> Result<(), Error> {
+		send_email(BillPaidSuccessfullyEmail {
+			username: "username".to_owned(),
+			workspaceName: "workspaceName".to_owned(),
+			monthString: "month".to_owned(),
+			year: 2102,
+			cardAmountDeducted: 24356,
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_payment_failure_invoice_email_email() -> Result<(), Error> {
+		send_email(PaymentFailureInvoiceEmail {
+			username: "username".to_owned(),
+			workspaceName: "workspaceName".to_owned(),
+			billBreakdown: WorkspaceBillBreakdown {
+				year: 2022,
+				month: 5,
+				total_charge: 22400,
+				deployment_charge: 3200,
+				deployment_usage: BTreeMap::from([(
+					Uuid::parse_str("d5727fb4-9e6b-43df-8a46-0c698340fffb")
+						.unwrap(),
+					DeploymentUsage {
+						name: "test-deplo".to_string(),
+						bill_items: vec![DeploymentBills {
+							machine_type: DeploymentMachineType {
+								id: Uuid::parse_str(
+									"d5727fb4-9e6b-43df-8a46-0c698340fffb",
+								)
+								.unwrap(),
+								cpu_count: 1,
+								memory_count: 2,
+							},
+							num_instances: 2,
+							hours: 720,
+							amount: 3200,
+						}],
+					},
+				)]),
+				database_charge: 3200,
+				database_usage: BTreeMap::from([(
+					Uuid::parse_str("d5727fb4-9e6b-43df-8a46-0c698340fffb")
+						.unwrap(),
+					DatabaseUsage {
+						name: "mydb".to_string(),
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				static_site_charge: 3200,
+				static_site_usage: BTreeMap::from([(
+					StaticSitePlan::Pro,
+					StaticSiteUsage {
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				domain_charge: 3200,
+				domain_usage: BTreeMap::from([(
+					DomainPlan::Free,
+					DomainUsage {
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				managed_url_charge: 3200,
+				managed_url_usage: BTreeMap::from([(
+					5,
+					ManagedUrlUsage {
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				secret_charge: 3200,
+				secret_usage: BTreeMap::from([(
+					5,
+					SecretUsage {
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				docker_repository_charge: 3200,
+				docker_repository_usage: vec![DockerRepositoryUsage {
+					storage: 10,
+					hours: 720,
+					amount: 3200,
+				}],
+			},
+			billingAddress: Address {
+				first_name: String::from("John"),
+				last_name: String::from("Patr"),
+				address_line_1: "221B Baker St, Marylebone".to_string(),
+				address_line_2: None,
+				address_line_3: None,
+				city: "London".to_string(),
+				state: "Lincolnshire".to_string(),
+				zip: "NW1 6XE".to_string(),
+				country: "UK".to_string(),
+			},
+			monthString: "month".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_payment_success_invoice_email_email() -> Result<(), Error> {
+		send_email(PaymentSuccessInvoiceEmail {
+			username: "username".to_owned(),
+			workspaceName: "workspaceName".to_owned(),
+			billBreakdown: WorkspaceBillBreakdown {
+				year: 2022,
+				month: 5,
+				total_charge: 22400,
+				deployment_charge: 3200,
+				deployment_usage: BTreeMap::from([(
+					Uuid::parse_str("d5727fb4-9e6b-43df-8a46-0c698340fffb")
+						.unwrap(),
+					DeploymentUsage {
+						name: "test-deplo".to_string(),
+						bill_items: vec![DeploymentBills {
+							machine_type: DeploymentMachineType {
+								id: Uuid::parse_str(
+									"d5727fb4-9e6b-43df-8a46-0c698340fffb",
+								)
+								.unwrap(),
+								cpu_count: 1,
+								memory_count: 2,
+							},
+							num_instances: 2,
+							hours: 720,
+							amount: 3200,
+						}],
+					},
+				)]),
+				database_charge: 3200,
+				database_usage: BTreeMap::from([(
+					Uuid::parse_str("d5727fb4-9e6b-43df-8a46-0c698340fffb")
+						.unwrap(),
+					DatabaseUsage {
+						name: "mydb".to_string(),
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				static_site_charge: 3200,
+				static_site_usage: BTreeMap::from([(
+					StaticSitePlan::Pro,
+					StaticSiteUsage {
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				domain_charge: 3200,
+				domain_usage: BTreeMap::from([(
+					DomainPlan::Free,
+					DomainUsage {
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				managed_url_charge: 3200,
+				managed_url_usage: BTreeMap::from([(
+					5,
+					ManagedUrlUsage {
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				secret_charge: 3200,
+				secret_usage: BTreeMap::from([(
+					5,
+					SecretUsage {
+						hours: 720,
+						amount: 3200,
+					},
+				)]),
+				docker_repository_charge: 3200,
+				docker_repository_usage: vec![DockerRepositoryUsage {
+					storage: 10,
+					hours: 720,
+					amount: 3200,
+				}],
+			},
+			billingAddress: Address {
+				first_name: String::from("John"),
+				last_name: String::from("Patr"),
+				address_line_1: "221B Baker St, Marylebone".to_string(),
+				address_line_2: None,
+				address_line_3: None,
+				city: "London".to_string(),
+				state: "Lincolnshire".to_string(),
+				zip: "NW1 6XE".to_string(),
+				country: "UK".to_string(),
+			},
+			monthString: "month".to_owned(),
+			creditsDeducted: 25443,
+			cardAmountDeducted: 123423,
+			creditsRemaining: 45234,
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_resource_deleted_email_email() -> Result<(), Error> {
+		send_email(ResourceDeletedEmail {
+			workspaceName: "workspaceName".to_owned(),
+			resourceName: "resourceName".to_owned(),
+			username: "username".to_owned(),
+			deletedBy: "deletedBy".to_owned(),
+			resourceType: "resourceType".to_owned(),
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_domain_unverified_email() -> Result<(), Error> {
+		send_email(DomainUnverified {
+			domainName: "domainName".to_owned(),
+			domainId: "domainId".to_owned(),
+			username: "username".to_owned(),
+			isInternal: false,
+		})
+		.await
+	}
+
+	#[tokio::test]
+	async fn test_domain_verified_email() -> Result<(), Error> {
+		send_email(DomainVerified {
+			domainName: "domainName".to_owned(),
+			username: "username".to_owned(),
+			domainId: "domainId".to_owned(),
+		})
+		.await
+	}
 }
