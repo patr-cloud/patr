62378b6cf91f0f085d247dff9e9aec6e2dd9096e
Rakshith Ravi <rakshith.ravi@gmx.com>
2022-11-16 06:43:48 +0000

    Fixed handlebar extensions of shared folders as well

--- api/src/utils/handlebar_registry.rs
+++ api/src/utils/handlebar_registry.rs
@@ -1,4 +1,4 @@
-use std::{ffi::OsStr, fs};
+use std::fs;
 
 use handlebars::{handlebars_helper, Handlebars};
 use once_cell::sync::OnceCell;
@@ -8,8 +8,12 @@ use crate::utils::Error;
 static INSTANCE: OnceCell<Handlebars> = OnceCell::new();
 
 // helpers to use within handlebar templates
-handlebars_helper!(cents_to_dollars: |cents: u64| crate::utils::billing::cents_to_dollars(cents));
-handlebars_helper!(stringify_month: |month_in_num: u8| crate::utils::billing::stringify_month(month_in_num));
+handlebars_helper!(cents_to_dollars: |cents: u64| {
+	crate::utils::billing::cents_to_dollars(cents)
+});
+handlebars_helper!(stringify_month: |month_in_num: u8| {
+	crate::utils::billing::stringify_month(month_in_num)
+});
 
 fn initialize_handlebar_registry_helper<'a>() -> Result<Handlebars<'a>, Error> {
 	let mut handlebar = Handlebars::new();
@@ -24,17 +28,24 @@ fn initialize_handlebar_registry_helper<'a>() -> Result<Handlebars<'a>, Error> {
 	for entry in fs::read_dir(shared_template_folder)? {
 		let file_path = entry?.path();
 
-		if file_path.is_file() &&
-			file_path.extension() == Some(OsStr::new("hbs"))
-		{
-			if let Some(partial_name) = file_path
-				.file_stem()
-				.and_then(|file_name| file_name.to_str())
-			{
-				let file_content = fs::read_to_string(&file_path)?;
-				handlebar.register_partial(partial_name, file_content)?;
-			}
+		if !file_path.is_file() {
+			continue;
 		}
+
+		let Some("hbs" | "handlebars") = file_path
+			.extension()
+			.and_then(|osstr| osstr.to_str()) else {
+			continue;
+		};
+
+		let Some(partial_name) = file_path
+			.file_stem()
+			.and_then(|file_name| file_name.to_str()) else {
+			continue;
+		};
+
+		let file_content = fs::read_to_string(&file_path)?;
+		handlebar.register_partial(partial_name, file_content)?;
 	}
 
 	Ok(handlebar)
