0d70f44f93120eebf682545fbe3cde61e2bc78ed
ashish.oli <ashish.oli@vicara.co>
2022-12-20 15:08:33 +0000

    rebased with upstream develop, fix migrations

--- api/src/migrations/from_v0/from_v0_12/from_v0_12_1.rs
+++ api/src/migrations/from_v0/from_v0_12/from_v0_12_1.rs
@@ -1,11 +1,30 @@
 use crate::{
+	migrate_query as query,
 	utils::{settings::Settings, Error},
 	Database,
 };
 
 pub(super) async fn migrate(
-	_connection: &mut <Database as sqlx::Database>::Connection,
+	connection: &mut <Database as sqlx::Database>::Connection,
+	config: &Settings,
+) -> Result<(), Error> {
+	managed_url_redirects(connection, config).await?;
+	Ok(())
+}
+
+pub(super) async fn managed_url_redirects(
+	connection: &mut <Database as sqlx::Database>::Connection,
 	_config: &Settings,
 ) -> Result<(), Error> {
+	query!(
+		r#"
+		ALTER TABLE managed_url
+		ADD COLUMN permanent_redirect BOOLEAN,
+		ADD COLUMN http_only BOOLEAN;
+		"#
+	)
+	.execute(&mut *connection)
+	.await?;
+
 	Ok(())
 }
