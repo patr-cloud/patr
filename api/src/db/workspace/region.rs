81bca94c1d2e187a1081626cb5a7d9ee0147b342
kavin <kavin.raj@vicara.co>
2022-12-23 15:12:08 +0000

    Added support for kubernetes database

--- api/src/db/workspace/region.rs
+++ api/src/db/workspace/region.rs
@@ -48,6 +48,14 @@ impl Region {
 	pub fn is_ready(&self) -> bool {
 		self.ready || self.is_patr_region()
 	}
+
+	pub fn is_patr_region(&self) -> bool {
+		self.workspace_id.is_none()
+	}
+
+	pub fn is_ready(&self) -> bool {
+		self.ready || self.is_patr_region()
+	}
 }
 
 pub async fn initialize_region_pre(
