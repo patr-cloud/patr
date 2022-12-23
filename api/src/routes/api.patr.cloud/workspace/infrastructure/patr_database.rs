e67c510bace47470fa5b507c69f8cb7cf3068bb4
kavin <kavin.raj@vicara.co>
2022-12-23 15:12:08 +0000

    Added support for kubernetes database

---
+++ api/src/routes/api.patr.cloud/workspace/infrastructure/patr_database.rs
@@ -0,0 +1,381 @@
+use api_macros::closure_as_pinned_box;
+use api_models::{
+	models::workspace::infrastructure::database::{
+		Connection,
+		CreateDatabaseRequest,
+		CreateDatabaseResponse,
+		Database,
+		DeleteDatabaseResponse,
+		GetDatabaseInfoResponse,
+		ListDatabasesResponse,
+	},
+	utils::Uuid,
+};
+use eve_rs::{App as EveApp, AsError, Context, NextHandler};
+
+use crate::{
+	app::{create_eve_app, App},
+	db,
+	error,
+	models::{rbac::permissions, ResourceType},
+	pin_fn,
+	service,
+	utils::{
+		constants::request_keys,
+		Error,
+		ErrorData,
+		EveContext,
+		EveMiddleware,
+	},
+};
+
+pub fn create_sub_app(
+	app: &App,
+) -> EveApp<EveContext, EveMiddleware, App, ErrorData> {
+	let mut app = create_eve_app(app);
+	app.get(
+		"/",
+		[
+			EveMiddleware::ResourceTokenAuthenticator {
+				is_api_token_allowed: true,
+				permission:
+					permissions::workspace::infrastructure::patr_database::LIST,
+				resource: closure_as_pinned_box!(|mut context| {
+					let workspace_id =
+						context.get_param(request_keys::WORKSPACE_ID).unwrap();
+					let workspace_id = Uuid::parse_str(workspace_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let resource = db::get_resource_by_id(
+						context.get_database_connection(),
+						&workspace_id,
+					)
+					.await?;
+
+					if resource.is_none() {
+						context
+							.status(404)
+							.json(error!(RESOURCE_DOES_NOT_EXIST));
+					}
+
+					Ok((context, resource))
+				}),
+			},
+			EveMiddleware::CustomFunction(pin_fn!(list_all_database_clusters)),
+		],
+	);
+
+	app.post(
+		"/",
+		[
+			EveMiddleware::ResourceTokenAuthenticator {
+				is_api_token_allowed: true,
+				permission: permissions::workspace::infrastructure::patr_database::CREATE,
+				resource: closure_as_pinned_box!(|mut context| {
+					let workspace_id =
+						context.get_param(request_keys::WORKSPACE_ID).unwrap();
+					let workspace_id = Uuid::parse_str(workspace_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let resource = db::get_resource_by_id(
+						context.get_database_connection(),
+						&workspace_id,
+					)
+					.await?;
+
+					if resource.is_none() {
+						context
+							.status(404)
+							.json(error!(RESOURCE_DOES_NOT_EXIST));
+					}
+
+					Ok((context, resource))
+				}),
+			},
+			EveMiddleware::CustomFunction(pin_fn!(create_database_cluster)),
+		],
+	);
+
+	app.get(
+		"/:databaseId/",
+		[
+			EveMiddleware::ResourceTokenAuthenticator {
+				is_api_token_allowed: true,
+				permission:
+					permissions::workspace::infrastructure::patr_database::INFO,
+				resource: closure_as_pinned_box!(|mut context| {
+					let workspace_id =
+						context.get_param(request_keys::WORKSPACE_ID).unwrap();
+					let workspace_id = Uuid::parse_str(workspace_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let database_id =
+						context.get_param(request_keys::DATABASE_ID).unwrap();
+					let database_id_string = Uuid::parse_str(database_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let resource = db::get_resource_by_id(
+						context.get_database_connection(),
+						&database_id_string,
+					)
+					.await?
+					.filter(|value| value.owner_id == workspace_id);
+
+					if resource.is_none() {
+						context
+							.status(404)
+							.json(error!(RESOURCE_DOES_NOT_EXIST));
+					}
+
+					Ok((context, resource))
+				}),
+			},
+			EveMiddleware::CustomFunction(pin_fn!(get_patr_database_info)),
+		],
+	);
+
+	app.delete(
+		"/:databaseId/",
+		[
+			EveMiddleware::ResourceTokenAuthenticator {
+				is_api_token_allowed: true,
+				permission: permissions::workspace::infrastructure::patr_database::DELETE,
+				resource: closure_as_pinned_box!(|mut context| {
+					let workspace_id =
+						context.get_param(request_keys::WORKSPACE_ID).unwrap();
+					let workspace_id = Uuid::parse_str(workspace_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let database_id =
+						context.get_param(request_keys::DATABASE_ID).unwrap();
+					let database_id_string = Uuid::parse_str(database_id)
+						.status(400)
+						.body(error!(WRONG_PARAMETERS).to_string())?;
+
+					let resource = db::get_resource_by_id(
+						context.get_database_connection(),
+						&database_id_string,
+					)
+					.await?
+					.filter(|value| value.owner_id == workspace_id);
+
+					if resource.is_none() {
+						context
+							.status(404)
+							.json(error!(RESOURCE_DOES_NOT_EXIST));
+					}
+
+					Ok((context, resource))
+				}),
+			},
+			EveMiddleware::CustomFunction(pin_fn!(delete_patr_database)),
+		],
+	);
+	app
+}
+
+async fn list_all_database_clusters(
+	mut context: EveContext,
+	_: NextHandler<EveContext, ErrorData>,
+) -> Result<EveContext, Error> {
+	let request_id = Uuid::new_v4();
+	let workspace_id =
+		Uuid::parse_str(context.get_param(request_keys::WORKSPACE_ID).unwrap())
+			.unwrap();
+
+	log::trace!(
+		"request_id: {} - Getting all database cluster info from db",
+		request_id
+	);
+	let database_clusters = db::get_all_patr_database_for_workspace(
+		context.get_database_connection(),
+		&workspace_id,
+	)
+	.await?
+	.into_iter()
+	.map(|database| Database {
+		id: database.id,
+		name: database.name,
+		database_name: database.db_name,
+		engine: database.engine,
+		version: database.version,
+		database_plan: database.database_plan,
+		region: database.region,
+		status: database.status,
+		connection: Connection {
+			host: database.host,
+			port: database.port,
+			username: database.username,
+			password: database.password,
+		},
+	})
+	.collect::<Vec<_>>();
+
+	log::trace!(
+		"request_id: {} - Returning all database cluster info",
+		request_id
+	);
+
+	context.success(ListDatabasesResponse {
+		databases: database_clusters,
+	});
+
+	Ok(context)
+}
+
+async fn create_database_cluster(
+	mut context: EveContext,
+	_: NextHandler<EveContext, ErrorData>,
+) -> Result<EveContext, Error> {
+	let request_id = Uuid::new_v4();
+	let workspace_id =
+		Uuid::parse_str(context.get_param(request_keys::WORKSPACE_ID).unwrap())
+			.unwrap();
+	let config = context.get_state().config.clone();
+
+	log::trace!("request_id: {} - Creating database cluster", request_id);
+	let CreateDatabaseRequest {
+		// use workspace_id from query param as this value will be default
+		workspace_id: _,
+		name,
+		database_name,
+		engine,
+		database_plan,
+		region,
+	} = context
+		.get_body_as()
+		.status(400)
+		.body(error!(WRONG_PARAMETERS).to_string())?;
+
+	let database_id = service::create_patr_database_in_workspace(
+		context.get_database_connection(),
+		&name,
+		&database_name,
+		&engine,
+		&database_plan,
+		&region,
+		&workspace_id,
+		&config,
+		&request_id,
+	)
+	.await?;
+
+	context.commit_database_transaction().await?;
+
+	service::queue_check_and_update_database_status(
+		&workspace_id,
+		&database_id,
+		&config,
+		&request_id,
+	)
+	.await?;
+
+	let _ = service::get_internal_metrics(
+		context.get_database_connection(),
+		"A patr database instance has been created",
+	)
+	.await;
+
+	context.success(CreateDatabaseResponse { id: database_id });
+	Ok(context)
+}
+
+async fn get_patr_database_info(
+	mut context: EveContext,
+	_: NextHandler<EveContext, ErrorData>,
+) -> Result<EveContext, Error> {
+	let request_id = Uuid::new_v4();
+
+	let database_id =
+		Uuid::parse_str(context.get_param(request_keys::DATABASE_ID).unwrap())
+			.unwrap();
+
+	log::trace!("request_id: {} - Getting database info", request_id);
+	let database = db::get_patr_database_by_id(
+		context.get_database_connection(),
+		&database_id,
+	)
+	.await?
+	.map(|database| Database {
+		id: database.id,
+		name: database.name,
+		database_name: database.db_name,
+		engine: database.engine,
+		version: database.version,
+		database_plan: database.database_plan,
+		region: database.region,
+		status: database.status,
+		connection: Connection {
+			host: database.host,
+			port: database.port,
+			username: database.username,
+			password: database.password,
+		},
+	})
+	.status(400)
+	.body(error!(WRONG_PARAMETERS).to_string())?;
+	log::trace!("request_id: {} - Returning database info", request_id);
+
+	context.success(GetDatabaseInfoResponse { database });
+
+	Ok(context)
+}
+
+async fn delete_patr_database(
+	mut context: EveContext,
+	_: NextHandler<EveContext, ErrorData>,
+) -> Result<EveContext, Error> {
+	let request_id = Uuid::new_v4();
+
+	let user_id = context.get_token_data().unwrap().user_id().clone();
+
+	let database_id =
+		Uuid::parse_str(context.get_param(request_keys::DATABASE_ID).unwrap())
+			.unwrap();
+
+	let database = db::get_patr_database_by_id(
+		context.get_database_connection(),
+		&database_id,
+	)
+	.await?
+	.status(404)
+	.body(error!(RESOURCE_DOES_NOT_EXIST).to_string())?;
+
+	let config = context.get_state().config.clone();
+
+	log::trace!("request_id: {} - Deleting database cluster", request_id);
+	service::delete_patr_database(
+		context.get_database_connection(),
+		&database_id,
+		&config,
+		&request_id,
+	)
+	.await?;
+
+	// Commiting transaction so that even if the mailing function fails the
+	// resource should be deleted
+	context.commit_database_transaction().await?;
+
+	service::resource_delete_action_email(
+		context.get_database_connection(),
+		&database.name,
+		&database.workspace_id,
+		&ResourceType::ManagedDatabase,
+		&user_id,
+	)
+	.await?;
+
+	let _ = service::get_internal_metrics(
+		context.get_database_connection(),
+		"A patr database instance has been deleted",
+	)
+	.await;
+
+	context.success(DeleteDatabaseResponse {});
+	Ok(context)
+}
---
+++ api/src/service/infrastructure/kubernetes/database/mod.rs
@@ -0,0 +1,71 @@
+mod mysql;
+
+use api_models::{
+	models::workspace::infrastructure::database::{
+		PatrDatabasePlan,
+		PatrDatabaseStatus,
+	},
+	utils::Uuid,
+};
+use k8s_openapi::{
+	api::apps::v1::StatefulSet,
+	apimachinery::pkg::api::resource::Quantity,
+};
+use kube::Api;
+pub use mysql::*;
+
+use crate::{service::KubernetesConfigDetails, utils::Error};
+
+pub trait ResourceLimitsForPlan {
+	fn get_resource_limits(&self) -> (Quantity, Quantity, Quantity);
+}
+
+impl ResourceLimitsForPlan for PatrDatabasePlan {
+	fn get_resource_limits(&self) -> (Quantity, Quantity, Quantity) {
+		let (ram, cpu, volume) = match self {
+			PatrDatabasePlan::db_1r_1c_10v => ("1Gi", "1", "10Gi"),
+			PatrDatabasePlan::db_2r_2c_25v => ("2Gi", "2", "25Gi"),
+		};
+
+		(
+			Quantity(ram.to_owned()),
+			Quantity(cpu.to_owned()),
+			Quantity(volume.to_owned()),
+		)
+	}
+}
+
+pub async fn get_kubernetes_database_status(
+	workspace_id: &Uuid,
+	database_id: &Uuid,
+	kubeconfig: KubernetesConfigDetails,
+	request_id: &Uuid,
+) -> Result<PatrDatabaseStatus, Error> {
+	let kubernetes_client =
+		super::get_kubernetes_client(kubeconfig.auth_details).await?;
+
+	// names
+	let namespace = workspace_id.as_str();
+	let sts_name_for_db = format!("db-{database_id}");
+
+	log::trace!("request_id: {request_id} - Getting statefulset statis for database {database_id}");
+	let sts =
+		Api::<StatefulSet>::namespaced(kubernetes_client.clone(), namespace)
+			.get_opt(&sts_name_for_db)
+			.await?;
+
+	let ready_replicas = match sts
+		.and_then(|sts| sts.status)
+		.and_then(|status| status.ready_replicas)
+	{
+		Some(ready_replicas) => ready_replicas,
+		None => return Ok(PatrDatabaseStatus::Errored),
+	};
+
+	// todo: need to change when database cluster is used
+	if ready_replicas == 1 {
+		Ok(PatrDatabaseStatus::Running)
+	} else {
+		Ok(PatrDatabaseStatus::Creating)
+	}
+}
