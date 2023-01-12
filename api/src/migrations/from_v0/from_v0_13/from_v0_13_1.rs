dbb76187a94d744bc42f144973a6d95db5b64537
ashish.oli <ashish.oli@vicara.co>
2023-01-12 14:20:15 +0000

    fix: image_name validation regex and ran sqlx prepare

--- api/src/migrations/from_v0/from_v0_13/from_v0_13_1.rs
+++ api/src/migrations/from_v0/from_v0_13/from_v0_13_1.rs
@@ -8,7 +8,16 @@ pub(super) async fn migrate(
 	connection: &mut <Database as sqlx::Database>::Connection,
 	config: &Settings,
 ) -> Result<(), Error> {
+	delete_deployment_with_invalid_image_name(connection, config).await?;
 	validate_image_name_for_deployment(connection, config).await?;
+	Ok(())
+}
+
+pub(super) async fn delete_deployment_with_invalid_image_name(
+	connection: &mut <Database as sqlx::Database>::Connection,
+	config: &Settings,
+) -> Result<(), Error> {
+	
 	Ok(())
 }
 
@@ -21,7 +30,7 @@ pub(super) async fn validate_image_name_for_deployment(
 		ALTER TABLE deployment
 		ADD CONSTRAINT deployment_chk_image_name_is_valid
 		CHECK (
-			image_name::TEXT ~* '^(([a-z0-9]+)(((?:[._]|__|[-]*)([a-z0-9]+))*)?)(((\/)(([a-z0-9]+)(((?:[._]|__|[-]*)([a-z0-9]+))*)?))*)?$'
+			image_name::TEXT ~ '^(([a-z0-9]+)(((?:[._]|__|[-]*)([a-z0-9]+))*)?)(((\/)(([a-z0-9]+)(((?:[._]|__|[-]*)([a-z0-9]+))*)?))*)?$'
 		);
 	"#
 	)
