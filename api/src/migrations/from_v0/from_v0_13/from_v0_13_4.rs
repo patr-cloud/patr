2ca98d1a2f22d4ac0c2bdfb4bb25bb9713993c88
Ankesh S <ankesh.singh@vicara.co>
2023-01-24 08:35:21 +0000

    Moved changes to current migration

--- api/src/migrations/from_v0/from_v0_13/from_v0_13_4.rs
+++ api/src/migrations/from_v0/from_v0_13/from_v0_13_4.rs
@@ -1,11 +1,46 @@
 use crate::{
+	migrate_query as query,
 	utils::{settings::Settings, Error},
 	Database,
 };
 
 pub(super) async fn migrate(
-	_connection: &mut <Database as sqlx::Database>::Connection,
+	connection: &mut <Database as sqlx::Database>::Connection,
+	config: &Settings,
+) -> Result<(), Error> {
+	reset_invalid_birthday(&mut *connection, config).await?;
+	Ok(())
+}
+
+pub(super) async fn reset_invalid_birthday(
+	connection: &mut <Database as sqlx::Database>::Connection,
 	_config: &Settings,
 ) -> Result<(), Error> {
+	log::info!("Updating all invalid user dob");
+
+	query!(
+		r#"
+		UPDATE
+			"user"
+		SET
+			dob = NULL
+		WHERE
+			dob IS NOT NULL AND
+			dob > (NOW() - INTERVAL '13 YEARS');
+		"#,
+	)
+	.execute(&mut *connection)
+	.await?;
+
+	log::info!("All invalid dobs updated");
+
+	query!(
+		r#"
+		ALTER TABLE "user" ADD CONSTRAINT user_chk_dob_is_13_plus CHECK (dob IS NULL OR dob < (NOW() - INTERVAL '13 YEARS'));
+		"#
+	)
+	.execute(&mut *connection)
+	.await?;
+
 	Ok(())
 }
