411fbd306bb3bd63aeb12c0f15423eaa10807c80
kavin <kavin.raj@vicara.co>
2023-03-21 10:45:03 +0000

    Added specific attempts for forgot password otp

--- api/src/migrations/from_v0/from_v0_15/from_v0_15_1.rs
+++ api/src/migrations/from_v0/from_v0_15/from_v0_15_1.rs
@@ -9,6 +9,7 @@ pub(super) async fn migrate(
 	config: &Settings,
 ) -> Result<(), Error> {
 	migrate_ci_builds(connection, config).await?;
+	add_attempts_for_password_reset(connection, config).await?;
 	Ok(())
 }
 
@@ -20,6 +21,33 @@ async fn migrate_ci_builds(
 		r#"
 		ALTER TABLE ci_builds
 		ALTER COLUMN author DROP NOT NULL;
+		"#
+	)
+	.execute(&mut *connection)
+	.await?;
+
+	Ok(())
+}
+
+async fn add_attempts_for_password_reset(
+	connection: &mut <Database as sqlx::Database>::Connection,
+	_config: &Settings,
+) -> Result<(), Error> {
+	query!(
+		r#"
+		ALTER TABLE password_reset_request
+		ADD COLUMN attempts INT NOT NULL DEFAULT 0
+			CONSTRAINT password_reset_request_attempts_non_negative
+				CHECK(attempts >= 0);
+		"#
+	)
+	.execute(&mut *connection)
+	.await?;
+
+	query!(
+		r#"
+		ALTER TABLE password_reset_request
+		ALTER COLUMN attempts DROP DEFAULT;
 		"#
 	)
 	.execute(&mut *connection)
