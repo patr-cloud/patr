f89bb8ab9045fdd6a5a2cc2b3fa60967c4b2fc15
Ankesh S <ankesh.singh@vicara.co>
2023-04-12 08:05:54 +0000

    Updated migrations

--- api/src/migrations/from_v0/from_v0_15/from_v0_15_1.rs
+++ api/src/migrations/from_v0/from_v0_15/from_v0_15_1.rs
@@ -10,6 +10,9 @@ pub(super) async fn migrate(
 ) -> Result<(), Error> {
 	migrate_ci_builds(connection, config).await?;
 	add_attempts_for_password_reset(connection, config).await?;
+	add_oauth_column_to_user_table(&mut *connection, config).await?;
+	add_oauth_column_to_user_to_sign_up_table(&mut *connection, config).await?;
+
 	Ok(())
 }
 
@@ -53,5 +56,35 @@ async fn add_attempts_for_password_reset(
 	.execute(&mut *connection)
 	.await?;
 
+	Ok(())
+}
+
+async fn add_oauth_column_to_user_table(
+	connection: &mut <Database as sqlx::Database>::Connection,
+	_config: &Settings,
+) -> Result<(), Error> {
+	query!(
+		r#"
+		ALTER TABLE "user"
+			ADD COLUMN is_oauth_user BOOLEAN DEFAULT false;
+		"#
+	)
+	.execute(&mut *connection)
+	.await?;
+	Ok(())
+}
+
+async fn add_oauth_column_to_user_to_sign_up_table(
+	connection: &mut <Database as sqlx::Database>::Connection,
+	_config: &Settings,
+) -> Result<(), Error> {
+	query!(
+		r#"
+		ALTER TABLE user_to_sign_up
+			ADD COLUMN is_oauth_user BOOLEAN DEFAULT false;
+		"#
+	)
+	.execute(&mut *connection)
+	.await?;
 	Ok(())
 }
