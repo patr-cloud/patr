44d0fa56582a280c80b7361bf9df874598d7d610
ashish.oli <ashish.oli@vicara.co>
2023-04-25 08:59:26 +0000

    feat: schedular for report card

--- api/src/scheduler/report_card.rs
+++ api/src/scheduler/report_card.rs
@@ -4,13 +4,18 @@ use api_models::utils::DateTime;
 use chrono::{Datelike, TimeZone, Utc};
 
 use super::Job;
-use crate::{db, models::UserDeployment, service, utils::Error};
+use crate::{
+	db,
+	models::{deployment, UserDeployment},
+	service,
+	utils::Error,
+};
 
 // Every two hours
 pub(super) fn generate_report_card_job() -> Job {
 	Job::new(
 		String::from("Update bills of workspaces"),
-		"0 0 1/2 * * *".parse().unwrap(),
+		"0 0 */15 * * *".parse().unwrap(),
 		|| Box::pin(generate_report_card()),
 	)
 }
@@ -28,26 +33,33 @@ async fn generate_report_card() -> Result<(), Error> {
 		let mut connection =
 			super::CONFIG.get().unwrap().database.begin().await?;
 		let deployments =
-			db::get_deployments_for_workspace(&mut connection, &workspace.id)
+			db::get_deployments_for_report_card(&mut connection, &workspace.id)
 				.await?;
 
-		let user_deployment = Vec::new();
+		let mut user_deployment = Vec::new();
 		for deployment in deployments {
-			let machint_type = db::get_machine_type_id(
+			let machine_type = db::get_machine_type_id(
 				&mut connection,
 				&deployment.machine_type,
 			)
 			.await?;
 
-			let deployment_usages = db::get_all_deployment_usage(
+			let deployment_usages = db::get_deployment_usage(
 				&mut *connection,
-				&workspace.id,
+				&deployment.id,
 				&DateTime::from(month_start_date),
 				&Utc::now(),
 			)
 			.await?;
 
 			let mut hours = 0;
+			let mut monthly_price = 0f32;
+			let mut cpu_count = 0;
+			let mut memory_count = 0;
+			let remaining_free_hours = 720;
+			let price_in_cents = 0;
+			let mut plan = String::new();
+			let mut estimated_cost = 0.0;
 			for deployment_usage in deployment_usages {
 				let stop_time = deployment_usage
 					.stop_time
@@ -60,24 +72,49 @@ async fn generate_report_card() -> Result<(), Error> {
 					((stop_time - start_time).num_seconds() as f64 / 3600f64)
 						.ceil() as i64,
 				) as u64;
+
+				let (cpu, memory) = deployment::MACHINE_TYPES
+					.get()
+					.unwrap()
+					.get(&deployment_usage.machine_type)
+					.unwrap_or(&(1, 2));
+
+				cpu_count = *cpu as u32;
+				memory_count = *memory as u32;
+
+				monthly_price = match (cpu_count, memory_count) {
+					(1, 2) => 5f32,
+					(1, 4) => 10f32,
+					(1, 8) => 20f32,
+					(2, 8) => 40f32,
+					(4, 32) => 80f32,
+					_ => 0f32,
+				};
+
+				if (cpu_count, memory_count) == (1, 2) &&
+					deployment_usage.num_instance == 1
+				{
+					// Free eligible
+					plan = "Free".to_string();
+					estimated_cost = 0.0 as f32;
+				} else {
+					plan = monthly_price.to_string();
+					estimated_cost = monthly_price as f32 *
+						deployment_usage.num_instance as f32;
+				}
+
+				break;
 			}
-
-			// TODO get plan based on deployment
-			let plan = todo!();
-
-			// TODO - estimated cost
-			let estimatedCost = todo!();
 
 			user_deployment.push(UserDeployment {
 				deployment_id: deployment.id,
 				deployment_name: deployment.name,
 				hours,
 				instances: deployment.max_horizontal_scale as u64,
-				estimated_cost: 15.0, // TODO - Change this after calculation
-				ram_count: machint_type.memory_count as u32,
-				cpu_count: machint_type.cpu_count as u32,
-				plan: "Free".to_string(), /* TODO -  change  plan after
-				                           * calculation */
+				estimated_cost,
+				ram_count: machine_type.memory_count as u32,
+				cpu_count: machine_type.cpu_count as u32,
+				plan,
 			})
 		}
 
