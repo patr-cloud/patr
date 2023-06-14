d6685df6b860a844fa42db26b16604a7e49476ea
Rakshith Ravi <rakshith.ravi@gmx.com>
2023-06-14 12:45:14 +0000

    Fixed reset permission order

--- api/src/migrations/from_v0/from_v0_16/from_v0_16_1.rs
+++ api/src/migrations/from_v0/from_v0_16/from_v0_16_1.rs
@@ -10,6 +10,7 @@ pub(super) async fn migrate(
 ) -> Result<(), Error> {
 	update_permissions(connection, config).await?;
 	add_rbac_blocklist_tables(connection, config).await?;
+	reset_permission_order(connection, config).await?;
 
 	Ok(())
 }
@@ -646,6 +647,125 @@ async fn add_rbac_blocklist_tables(
 	)
 	.execute(&mut *connection)
 	.await?;
+
+	Ok(())
+}
+
+async fn reset_permission_order(
+	connection: &mut <Database as sqlx::Database>::Connection,
+	_config: &Settings,
+) -> Result<(), Error> {
+	for permission in [
+		"workspace::domain::list",
+		"workspace::domain::add",
+		"workspace::domain::viewDetails",
+		"workspace::domain::verify",
+		"workspace::domain::delete",
+		"workspace::domain::dnsRecord::list",
+		"workspace::domain::dnsRecord::add",
+		"workspace::domain::dnsRecord::edit",
+		"workspace::domain::dnsRecord::delete",
+		"workspace::infrastructure::deployment::list",
+		"workspace::infrastructure::deployment::create",
+		"workspace::infrastructure::deployment::info",
+		"workspace::infrastructure::deployment::delete",
+		"workspace::infrastructure::deployment::edit",
+		"workspace::infrastructure::managedUrl::list",
+		"workspace::infrastructure::managedUrl::create",
+		"workspace::infrastructure::managedUrl::edit",
+		"workspace::infrastructure::managedUrl::delete",
+		"workspace::infrastructure::managedDatabase::create",
+		"workspace::infrastructure::managedDatabase::list",
+		"workspace::infrastructure::managedDatabase::delete",
+		"workspace::infrastructure::managedDatabase::info",
+		"workspace::infrastructure::staticSite::list",
+		"workspace::infrastructure::staticSite::create",
+		"workspace::infrastructure::staticSite::info",
+		"workspace::infrastructure::staticSite::delete",
+		"workspace::infrastructure::staticSite::edit",
+		"workspace::containerRegistry::create",
+		"workspace::containerRegistry::list",
+		"workspace::containerRegistry::delete",
+		"workspace::containerRegistry::info",
+		"workspace::containerRegistry::push",
+		"workspace::containerRegistry::pull",
+		"workspace::secret::list",
+		"workspace::secret::create",
+		"workspace::secret::edit",
+		"workspace::secret::delete",
+		"workspace::rbac::role::list",
+		"workspace::rbac::role::create",
+		"workspace::rbac::role::edit",
+		"workspace::rbac::role::delete",
+		"workspace::rbac::user::list",
+		"workspace::rbac::user::add",
+		"workspace::rbac::user::remove",
+		"workspace::rbac::user::updateRoles",
+		"workspace::region::list",
+		"workspace::region::info",
+		"workspace::region::check_status",
+		"workspace::region::add",
+		"workspace::region::delete",
+		"workspace::ci::recentActivity",
+		"workspace::ci::gitProvider::list",
+		"workspace::ci::gitProvider::connect",
+		"workspace::ci::gitProvider::disconnect",
+		"workspace::ci::gitProvider::repo::activate",
+		"workspace::ci::gitProvider::repo::deactivate",
+		"workspace::ci::gitProvider::repo::list",
+		"workspace::ci::gitProvider::repo::info",
+		"workspace::ci::gitProvider::repo::write",
+		"workspace::ci::gitProvider::repo::build::list",
+		"workspace::ci::gitProvider::repo::build::cancel",
+		"workspace::ci::gitProvider::repo::build::info",
+		"workspace::ci::gitProvider::repo::build::start",
+		"workspace::ci::gitProvider::repo::build::restart",
+		"workspace::ci::runner::list",
+		"workspace::ci::runner::create",
+		"workspace::ci::runner::info",
+		"workspace::ci::runner::update",
+		"workspace::ci::runner::delete",
+		"workspace::billing::info",
+		"workspace::billing::makePayment",
+		"workspace::billing::paymentMethod::add",
+		"workspace::billing::paymentMethod::delete",
+		"workspace::billing::paymentMethod::list",
+		"workspace::billing::paymentMethod::edit",
+		"workspace::billing::billingAddress::add",
+		"workspace::billing::billingAddress::delete",
+		"workspace::billing::billingAddress::info",
+		"workspace::billing::billingAddress::edit",
+		"workspace::edit",
+		"workspace::delete",
+	] {
+		query!(
+			r#"
+			UPDATE
+				permission
+			SET
+				name = CONCAT('test::', name)
+			WHERE
+				name = $1;
+			"#,
+			permission,
+		)
+		.execute(&mut *connection)
+		.await?;
+
+		query!(
+			r#"
+			UPDATE
+				permission
+			SET
+				name = $1
+			WHERE
+				name = CONCAT('test::', $1);
+			"#,
+			&permission,
+		)
+		.execute(&mut *connection)
+		.await?;
+	}
 
 	Ok(())
 }
