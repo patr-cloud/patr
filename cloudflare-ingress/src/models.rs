8582cdfdd7e1e3babe18052cf0483a3d70f59da1
Rakshith Ravi <rakshith.ravi@gmx.com>
2023-07-25 18:35:31 +0530

    Cloudflare ingress done. Todo check how to do partial content

---
+++ cloudflare-ingress/src/models.rs
@@ -0,0 +1,31 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum IngressKVData {
+	#[serde(rename_all = "camelCase")]
+	Redirect {
+		to: String,
+		permanent_redirect: bool,
+		http_only: bool,
+	},
+	#[serde(rename_all = "camelCase")]
+	Proxy { to: String, http_only: bool },
+	#[serde(rename_all = "camelCase")]
+	StaticSite {
+		static_site_id: String,
+		upload_id: String,
+	},
+	#[serde(rename_all = "camelCase")]
+	Deployment {
+		deployment_id: String,
+		port: u16,
+		region: String,
+	},
+}
+
+impl IngressKVData {
+	pub fn is_redirect(&self) -> bool {
+		matches!(self, IngressKVData::Redirect { .. })
+	}
+}
