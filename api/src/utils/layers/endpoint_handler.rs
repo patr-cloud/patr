f0663383b6621a63db2f0fd1ae272a9c255949cd
Rakshith Ravi <rakshith.ravi@gmx.com>
2023-09-02 12:37:17 +0000

    Added a authenticated endpoint mounter

--- api/src/utils/layers/endpoint_handler.rs
+++ api/src/utils/layers/endpoint_handler.rs
@@ -4,19 +4,18 @@ use std::{
 	task::{Context, Poll},
 };
 
-use models::{ApiEndpoint, ErrorType, utils::{False, True}};
+use models::{ApiEndpoint, ErrorType};
 use tower::{Layer, Service};
 
-use crate::{app::AppResponse, prelude::{AppRequest, AuthenticatedAppRequest}};
+use crate::{app::AppResponse, prelude::AppRequest};
 
 pub trait EndpointHandler<E>
 where
 	E: ApiEndpoint,
 {
 	type Future: Future<Output = Result<AppResponse<E>, ErrorType>> + Send + 'static;
-	type Request<'a>;
 
-	fn call(self, req: Self::Request<'_>) -> Self::Future;
+	fn call<'a>(self, req: AppRequest<'a, E>) -> Self::Future;
 }
 
 impl<F, Fut, E> EndpointHandler<E> for F
@@ -24,27 +23,10 @@ where
 	F: FnOnce(AppRequest<'_, E>) -> Fut + Clone + Send + 'static,
 	Fut: Future<Output = Result<AppResponse<E>, ErrorType>> + Send + 'static,
 	E: ApiEndpoint,
-	E::AUTHD: Into<False>,
 {
 	type Future = Fut;
-	type Request<'a> = AppRequest<'a, E>;
 
-	fn call(self, req: Self::Request<'_>) -> Self::Future {
-		self(req)
-	}
-}
-
-impl<F, Fut, E> EndpointHandler<E> for F
-where
-	F: FnOnce(AuthenticatedAppRequest<'_, E>) -> Fut + Clone + Send + 'static,
-	Fut: Future<Output = Result<AppResponse<E>, ErrorType>> + Send + 'static,
-	E: ApiEndpoint,
-	E::AUTHD: Into<True>,
-{
-	type Future = Fut;
-	type Request<'a> = AuthenticatedAppRequest<'a, E>;
-
-	fn call(self, req: Self::Request<'_>) -> Self::Future {
+	fn call(self, req: AppRequest<'_, E>) -> Self::Future {
 		self(req)
 	}
 }
@@ -76,10 +58,10 @@ where
 	H: EndpointHandler<E> + Clone + Send + 'static,
 	E: ApiEndpoint,
 {
-	type Service = EndpointService<H, E>;
+	type Service = AuthEndpointService<H, E>;
 
 	fn layer(&self, _: S) -> Self::Service {
-		EndpointService {
+		AuthEndpointService {
 			handler: self.handler.clone(),
 			endpoint: PhantomData,
 		}
@@ -99,7 +81,7 @@ where
 	}
 }
 
-pub struct EndpointService<H, E>
+pub struct AuthEndpointService<H, E>
 where
 	H: EndpointHandler<E> + Clone + Send + 'static,
 	E: ApiEndpoint,
@@ -108,7 +90,7 @@ where
 	endpoint: PhantomData<E>,
 }
 
-impl<'a, H, E> Service<AppRequest<'a, E>> for EndpointService<H, E>
+impl<'a, H, E> Service<AppRequest<'a, E>> for AuthEndpointService<H, E>
 where
 	H: EndpointHandler<E> + Clone + Send + 'static,
 	E: ApiEndpoint,
@@ -126,7 +108,7 @@ where
 	}
 }
 
-impl<H, E> Clone for EndpointService<H, E>
+impl<H, E> Clone for AuthEndpointService<H, E>
 where
 	H: EndpointHandler<E> + Clone + Send + 'static,
 	E: ApiEndpoint,
