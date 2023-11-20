26210a3761e73e59f4e794e9d7adaeca56b1ce70
Ankesh S <ankesh.singh@vicara.co.in>
2023-11-20 12:54:58 +0530

    Added models for secret

--- models/src/api/workspace/secret/delete_secret.rs
+++ models/src/api/workspace/secret/delete_secret.rs
@@ -1,74 +1,21 @@
-use axum_extra::routing::TypedPath;
-use reqwest::Method;
-use serde::{Deserialize, Serialize};
+use crate::{prelude::*, utils::BearerToken};
 
-use crate::{utils::Uuid, ApiRequest};
-
-#[derive(
-	Eq,
-	Ord,
-	Hash,
-	Debug,
-	Clone,
-	Default,
-	TypedPath,
-	PartialEq,
-	Serialize,
-	PartialOrd,
-	Deserialize,
-)]
-#[typed_path("/workspace/:workspace_id/secret/:secret_id")]
-pub struct DeleteSecretPath {
-	pub workspace_id: Uuid,
-	pub secret_id: Uuid,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-#[serde(rename_all = "camelCase")]
-pub struct DeleteSecretRequest;
-
-impl ApiRequest for DeleteSecretRequest {
-	const METHOD: Method = Method::DELETE;
-	const IS_PROTECTED: bool = true;
-
-	type RequestPath = DeleteSecretPath;
-	type RequestQuery = ();
-	type RequestBody = ();
-	type Response = ();
-}
-
-#[cfg(test)]
-mod test {
-	use serde_test::{assert_tokens, Token};
-
-	use super::DeleteSecretRequest;
-	use crate::{ApiRequest, ApiResponse};
-
-	#[test]
-	fn assert_request_types() {
-		assert_tokens(
-			&DeleteSecretRequest,
-			&[Token::UnitStruct {
-				name: "DeleteSecretRequest",
-			}],
-		);
+macros::declare_api_endpoint!(
+	/// Route to delete a secret
+	DeleteSecret,
+	DELETE "/workspace/:workspace_id/secret/:secret_id" {
+		/// The ID of the workspace
+		pub workspace_id: Uuid,
+		/// The ID of the secret to be deleted
+		pub secret_id: Uuid,
+	},
+	request_headers = {
+		/// Token used to authorize user
+		pub authorization: BearerToken
+	},
+	authentication = {
+		AppAuthentication::<Self>::ResourcePermissionAuthenticator {
+			extract_resource_id: |req| req.path.secret_id
+		}
 	}
-
-	#[test]
-	fn assert_response_types() {
-		crate::assert_types::<<DeleteSecretRequest as ApiRequest>::Response>(());
-	}
-
-	#[test]
-	fn assert_success_response_types() {
-		assert_tokens(
-			&ApiResponse::success(()),
-			&[
-				Token::Map { len: None },
-				Token::Str("success"),
-				Token::Bool(true),
-				Token::MapEnd,
-			],
-		);
-	}
-}
+);
--- models/src/api/workspace/secret/mod.rs
+++ models/src/api/workspace/secret/mod.rs
@@ -1,7 +1,3 @@
-use serde::{Deserialize, Serialize};
-
-use crate::utils::Uuid;
-
 mod create_secret;
 mod delete_secret;
 mod list_secrets_for_workspace;
@@ -13,12 +9,3 @@ pub use self::{
 	list_secrets_for_workspace::*,
 	update_secret::*,
 };
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-#[serde(rename_all = "camelCase")]
-pub struct Secret {
-	pub id: Uuid,
-	pub name: String,
-	#[serde(skip_serializing_if = "Option::is_none")]
-	pub deployment_id: Option<Uuid>,
-}
