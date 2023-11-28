1c32643656ee974d4522096b610d5cca355d894e
Ankesh S <ankesh.singh@vicara.co.in>
2023-11-28 14:13:15 +0530

    Added routes for RBAC

--- api/src/routes/api.patr.cloud/workspace/mod.rs
+++ api/src/routes/api.patr.cloud/workspace/mod.rs
@@ -1,11 +1,11 @@
 use axum::Router;
-
 use crate::prelude::*;
 
 mod infrastructure;
 mod domain;
 mod region;
 mod secret;
+mod rbac;
 
 #[instrument(skip(state))]
 pub async fn setup_routes(state: &AppState) -> Router {
@@ -14,4 +14,5 @@ pub async fn setup_routes(state: &AppState) -> Router {
 	.merge(domain::setup_routes(state).await)
 	.merge(region::setup_routes(state).await)
 	.merge(secret::setup_routes(state).await)
+	.merge(rbac::setup_routes(state).await)
 }
