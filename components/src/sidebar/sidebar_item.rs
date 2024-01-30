6380ef0bd6919a3962d561fd8d4d552c97705bcc
Argha Chakrabarty <ac380012@gmail.com>
2024-01-30 14:55:46 +0530

    fixed unused class variable in sidebar, and added docs for sidebar_item

--- components/src/sidebar/sidebar_item.rs
+++ components/src/sidebar/sidebar_item.rs
@@ -2,24 +2,29 @@ use crate::imports::*;
 
 #[component]
 pub fn SidebarItem(
-    #[prop(into)]
-    link: MaybeSignal<LinkItem>,
-    #[prop(into, optional)]
-    class: MaybeSignal<String>,
+	/// Link info of the link item
+	#[prop(into)]
+	link: MaybeSignal<LinkItem>,
+	/// Additional classes to add to the sidebar if necessary
+	#[prop(into, optional)]
+	class: MaybeSignal<String>,
 ) -> impl IntoView {
-    view! {
-        <li class="sidebar-item full-width fc-fs-fs">
-            <Link style_variant=LinkStyleVariant::Plain class="btn full-width py-sm">
-                <img src={link.get().icon_src} alt={link.get().title} />
-                <span class="ml-md txt-md fc-fs-fs txt-left">
-                    <span class="pos-rel txt-md txt-left">
-                        {link.get().title}
-                    </span>
-                    <small class="txt-xxs txt-grey">
-                        {link.get().subtitle}
-                    </small>
-                </span>
-            </Link>
-        </li>
-    }
+	let class = move || format!("sidebar-item full-width fc-fs-fs {}", class.get());
+
+	view! {
+		/// Will add the class later
+		<li class=class>
+			<Link style_variant=LinkStyleVariant::Plain class="btn full-width py-sm">
+				<img src={link.get().icon_src} alt={link.get().title} />
+				<span class="ml-md txt-md fc-fs-fs txt-left">
+					<span class="pos-rel txt-md txt-left">
+						{link.get().title}
+					</span>
+					<small class="txt-xxs txt-grey">
+						{link.get().subtitle}
+					</small>
+				</span>
+			</Link>
+		</li>
+	}
 }
