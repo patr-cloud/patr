359e076bac479086934efe194e7c95fc45855393
Argha Chakrabarty <ac380012@gmail.com>
2024-02-26 12:32:08 +0530

    modified image history page, modified deployment scaling and logs pages and required components

---
+++ components/src/log_statement.rs
@@ -0,0 +1,42 @@
+use crate::imports::*;
+
+pub struct LogItem {
+	pub timestamp: String,
+	pub log: String,
+}
+
+#[component]
+pub fn LogStatement(
+	/// Additional class names to apply to the outer div, if any
+	#[prop(into, optional)]
+	class: MaybeSignal<String>,
+	/// The Log Details
+	log: LogItem,
+) -> impl IntoView {
+	let outer_div_class =
+		class.with(|cname| format!("txt-grey log-statement fr-fs-ct full-width {}", cname));
+
+	view! {
+		<div class=outer_div_class>
+			<Icon icon=IconType::ChevronRight size=Size::ExtraSmall color=Color::Grey />
+
+			<ToolTipContainer
+				tooltip_width=10.
+				label=view! {
+					<time class="w-fix-10">
+						// {getTimeFromNow(Date.parse(log.timestamp))}
+						"13:00"
+					</time>
+				}.into_view()
+			>
+				<time date_time="2008-02-14 20:00" class="txt-xxs">
+					// {new Date(log.timestamp).toUTCString()}
+					"12:00"
+				</time>
+			</ToolTipContainer>
+			" - "
+			<span class="px-sm">{log.log}</span>
+		</div>
+
+	}
+}
---
+++ hosted-frontend/src/pages/infrastructure/deployment/manage_deployment/logs.rs
@@ -0,0 +1,29 @@
+use crate::prelude::*;
+
+#[component]
+pub fn ManageDeploymentsLogs() -> impl IntoView {
+	let log = LogItem {
+		timestamp: "12:00".to_owned(),
+		log: "npm run build".to_owned(),
+	};
+
+	view! {
+		<div class="full-width full-height px-xl my-xl of-hidden">
+			<div class="full-width full-height px-md fc-fs-fs">
+				<div class="full-width full-height br-sm bg-secondary px-xl py-md fc-fs-fs of-auto">
+					<div class="full-width pb-xxs fr-sb-ct ul-light mb-xs gap-xl">
+						<Link>
+							"LOAD MORE"
+						</Link>
+						<p class="txt-grey txt-xss">
+							"Displaying logs since {logsSince}"
+						</p>
+					</div>
+
+					<LogStatement log={log} class="mb-xs" />
+				</div>
+			</div>
+		</div>
+
+	}
+}
