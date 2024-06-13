ec7660ef7d7ab3d7d2d288b505653fcec1f28799
Rakshith Ravi <rakshith.ravi@gmx.com>
2024-06-13 21:17:06 +0530

    Refactor RBAC module and remove unused rbac.rs file

--- api/src/models/mod.rs
+++ api/src/models/mod.rs
@@ -1,6 +1,4 @@
 /// Contains the struct that will be encoded in the JWT of the access token.
 pub mod access_token_data;
-/// All the data for permissions, resource types, etc.
-pub mod rbac;
 /// Contains all the structs that will be stored in Redis
 pub mod redis;
