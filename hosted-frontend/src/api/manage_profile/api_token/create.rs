e3b97f8d1db1eb94d6dfdca6e6fa8894b482f549
Rakshith Ravi <rakshith.ravi@gmx.com>
2024-08-30 00:30:00 +0530

    Removed comments

--- hosted-frontend/src/api/manage_profile/api_token/create.rs
+++ hosted-frontend/src/api/manage_profile/api_token/create.rs
@@ -14,38 +14,6 @@ pub async fn create_api_token(
 	let access_token = BearerToken::from_str(access_token.unwrap().as_str())
 		.map_err(|_| ServerFnError::WrappedServerError(ErrorType::MalformedAccessToken))?;
 
-	// let format = format_description!("[year]-[month]-[day]");
-	//
-	// let token_nbf = token_nbf
-	// 	.some_if_not_empty()
-	// 	.map(|nbf| {
-	// 		let date = Date::parse(nbf.as_str(), &format).map_err(|er| {
-	// 			logging::log!("{:#?}", er);
-	// 			ServerFnError::WrappedServerError(ErrorType::WrongParameters)
-	// 		})?;
-	//
-	// 		Ok::<OffsetDateTime, ServerFnError<ErrorType>>(
-	// 			datetime!(2020-01-01 0:00 UTC).replace_date(date),
-	// 		)
-	// 	})
-	// 	.transpose()?;
-	//
-	// let token_exp = token_exp
-	// 	.some_if_not_empty()
-	// 	.map(|exp| {
-	// 		let date = Date::parse(exp.as_str(), &format).map_err(|er| {
-	// 			logging::log!("{:#?}", er);
-	// 			ServerFnError::WrappedServerError(ErrorType::WrongParameters)
-	// 		})?;
-	//
-	// 		Ok::<OffsetDateTime, ServerFnError<ErrorType>>(
-	// 			datetime!(2020-01-01 0:00 UTC).replace_date(date),
-	// 		)
-	// 	})
-	// 	.transpose()?;
-	//
-	// logging::log!("date tokens: {:?} {:?}", token_exp, token_nbf);
-
 	let api_response = make_api_call::<CreateApiTokenRequest>(
 		ApiRequest::builder()
 			.path(CreateApiTokenPath)
