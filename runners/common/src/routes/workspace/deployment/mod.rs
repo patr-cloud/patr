3490f769d1432eec892a3cb0d5b86ee8f8b97a26
Rakshith Ravi <rakshith.ravi@gmx.com>
2024-09-10 19:58:37 +0530

    WIP delete and update route. Untested yet

--- runners/common/src/routes/workspace/deployment/mod.rs
+++ runners/common/src/routes/workspace/deployment/mod.rs
@@ -1,9 +1,11 @@
 use axum::Router;
 
 mod create_deployment;
+mod delete_deployment;
 mod list_deployment;
+mod update_deployment;
 
-use self::{create_deployment::*, list_deployment::*};
+use self::{create_deployment::*, delete_deployment::*, list_deployment::*, update_deployment::*};
 use crate::{prelude::*, utils::RouterExt};
 
 #[instrument(skip(state))]
@@ -11,6 +13,8 @@ pub async fn setup_routes<E>(state: &AppState<E>) -> Router
 where
 	E: RunnerExecutor + Clone,
 {
-	Router::new().mount_auth_endpoint(list_deployment, state)
+	Router::new()
+		.mount_auth_endpoint(list_deployment, state)
+		.mount_auth_endpoint(delete_deployment, state)
 	// .mount_endpoint(create_deployment, state)
 }
