24e0f669224074263bcd6dc5a5fc870bae101fcf
Rakshith Ravi <rakshith.ravi@gmx.com>
2024-09-23 15:49:04 +0530

    WIP ingress token for runner

--- api/src/routes/api.patr.cloud/workspace/runner/mod.rs
+++ api/src/routes/api.patr.cloud/workspace/runner/mod.rs
@@ -3,6 +3,7 @@ use axum::Router;
 use crate::prelude::*;
 
 mod add_runner_to_workspace;
+mod get_ingress_token_for_runner;
 mod get_runner_info;
 mod list_runners_for_workspace;
 mod remove_runner_from_workspace;
@@ -10,6 +11,7 @@ mod stream_runner_data_for_workspace;
 
 use self::{
 	add_runner_to_workspace::*,
+	get_ingress_token_for_runner::*,
 	get_runner_info::*,
 	list_runners_for_workspace::*,
 	remove_runner_from_workspace::*,
@@ -19,9 +21,10 @@ use self::{
 #[instrument(skip(state))]
 pub async fn setup_routes(state: &AppState) -> Router {
 	Router::new()
-		.mount_auth_endpoint(stream_runner_data_for_workspace, state)
 		.mount_auth_endpoint(add_runner_to_workspace, state)
-		.mount_auth_endpoint(remove_runner_from_workspace, state)
-		.mount_auth_endpoint(list_runners_for_workspace, state)
+		.mount_auth_endpoint(get_ingress_token_for_runner, state)
 		.mount_auth_endpoint(get_runner_info, state)
+		.mount_auth_endpoint(list_runners_for_workspace, state)
+		.mount_auth_endpoint(remove_runner_from_workspace, state)
+		.mount_auth_endpoint(stream_runner_data_for_workspace, state)
 }
