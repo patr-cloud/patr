7b1dc44a260a06a62e19ddf416bd8cb6ae3866b4
Rakshith Ravi <rakshith.ravi@gmx.com>
2024-10-03 05:08:37 +0000

    Add Cloudflare API module and models

--- api/src/models/mod.rs
+++ api/src/models/mod.rs
@@ -1,4 +1,6 @@
 /// Contains the struct that will be encoded in the JWT of the access token.
 pub mod access_token_data;
+/// Contains all the structs that will be used by Cloudflare
+pub mod cloudflare;
 /// Contains all the structs that will be stored in Redis
 pub mod redis;
