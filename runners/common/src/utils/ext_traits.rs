f690241f9b9c8c761b4b216af89a378252400859
Rakshith Ravi <rakshith.ravi@gmx.com>
2025-03-13 23:41:23 +0000

    Add tokio-util dependency and implement cancellation checks in futures

--- runners/common/src/utils/ext_traits.rs
+++ runners/common/src/utils/ext_traits.rs
@@ -1,6 +1,7 @@
 use std::future::Future;
 
 use futures::future::Either;
+use tokio_util::sync::CancellationToken;
 
 use crate::error::RunnerError;
 
@@ -53,22 +54,22 @@ pub trait Exitable<T> {
 	/// Returns the value of the future if it completes before the exit signal
 	/// is triggered. If the exit signal is triggered before the future
 	/// completes, then this function will return [`None`].
-	fn if_not_exit<E>(self, exit_signal: &mut E) -> impl Future<Output = Result<T, RunnerError>>
-	where
-		E: Future<Output = ()> + Unpin;
+	fn with_cancel_check(self) -> impl Future<Output = Result<T, RunnerError>>;
 }
 
 impl<T, F> Exitable<T> for F
 where
 	F: Future<Output = T>,
 {
-	async fn if_not_exit<E>(self, exit_signal: &mut E) -> Result<T, RunnerError>
-	where
-		E: Future<Output = ()> + Unpin,
-	{
-		futures::future::select(std::pin::pin!(self), exit_signal)
-			.await
-			.into_left()
-			.ok_or(RunnerError::ExitSignalReceived)
+	async fn with_cancel_check(self) -> Result<T, RunnerError> {
+		futures::future::select(
+			std::pin::pin!(self),
+			std::pin::pin!(crate::runner::GLOBAL_CANCEL_TOKEN
+				.get_or_init(CancellationToken::new)
+				.cancelled()),
+		)
+		.await
+		.into_left()
+		.ok_or(RunnerError::ExitSignalReceived)
 	}
 }
