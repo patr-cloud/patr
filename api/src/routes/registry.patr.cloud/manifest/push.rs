4cbdacd990534cc992ac4e425047fdfb78876f29
Argha Chakrabarty <argha.chakrabarty@girnarsoft.com>
2025-08-28 20:13:09 +0000

    add manifest upload route

--- api/src/routes/registry.patr.cloud/manifest/push.rs
+++ api/src/routes/registry.patr.cloud/manifest/push.rs
@@ -1,6 +1,6 @@
 use axum::{
 	body::Body,
-	extract::{Path, Query, State},
+	extract::{Path, State},
 	http::{HeaderMap, HeaderName, HeaderValue, StatusCode, header::InvalidHeaderValue},
 	response::IntoResponse,
 };
@@ -9,11 +9,11 @@ use oci_spec::distribution::ErrorCode;
 use serde::{Deserialize, Serialize};
 use tokio_util::compat::FuturesAsyncReadCompatExt;
 
+use super::super::Error;
 use crate::{
 	prelude::*,
 	routes::registry_patr_cloud::{
-		Error,
-		get_s3_object_name_for_blob,
+		get_s3_object_name_for_manifest,
 		internal_server_error_response,
 	},
 	utils::helper::{check_workspace, convert_oci_error, get_s3_bucket, preprocess_stuff},
@@ -29,79 +29,51 @@ pub struct PathParams {
 	/// The name of the repository
 	#[preprocess(regex = r"[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*")]
 	repo_name: String,
+	/// The digest of the blob
+	#[preprocess(trim)]
+	referrer: String,
 }
 
-#[preprocess::sync]
-/// The Query Parameters that are passed
-#[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
-pub struct QueryParams {
-	/// The Digest of the blob
-	#[preprocess(lowercase, trim)]
-	#[serde(default)]
-	digest: String,
-}
-
-/// Defines the `POST` route for `/blobs/uploads`. [end-4a](https://github.com/opencontainers/distribution-spec/blob/main/spec.md#endpoints) and [end-4b](https://github.com/opencontainers/distribution-spec/blob/main/spec.md#endpoints)
+/// Handles the `GET /v2/<name>/manifests/<reference>` route. i.e. Pushing a manifest. See [end-7](https://github.com/opencontainers/distribution-spec/blob/main/spec.md#endpoints) for more details
 #[axum::debug_handler]
 pub(super) async fn handle(
 	header: HeaderMap,
 	Path(path): Path<PathParams>,
-	Query(query): Query<QueryParams>,
 	State(state): State<AppState>,
 	body: Body,
 ) -> Result<impl IntoResponse, Error> {
 	let path = preprocess_stuff(path)?;
-	let query = preprocess_stuff(query)?;
 
 	let workspace_id = path.workspace_id;
 	check_workspace(workspace_id, state.clone()).await?;
 
-	let digest = query.digest;
-	if digest.is_empty() {
-		todo!();
+	// TODO: This SHOULD support tags, but we don't have that rn
+	let referrer = path.referrer;
+	if !(referrer.starts_with("sha256:") && referrer.len() == 71) {
+		return Err(convert_oci_error(
+			StatusCode::BAD_REQUEST,
+			ErrorCode::ManifestInvalid,
+			"Invalid referrer format".to_string(),
+		));
 	}
 
-	let mut database = state
-		.database
-		.begin()
-		.await
-		.map_err(internal_server_error_response)?;
-
-	// if !digest.to_string() {
-	// 	todo!("Create this later")
-	// }
-
-	let header_content_length = header
-		.get("Content-Length")
-		.ok_or_else(|| {
-			convert_oci_error(
-				StatusCode::BAD_REQUEST,
-				ErrorCode::BlobUploadInvalid,
-				"Content-Length header is required".to_string(),
-			)
-		})?
-		.to_str()
-		.map_err(internal_server_error_response)?;
-	let header_content_type = header
+	let content_type = header
 		.get("Content-Type")
 		.ok_or_else(|| {
 			convert_oci_error(
-				StatusCode::UNSUPPORTED_MEDIA_TYPE,
-				ErrorCode::BlobUploadInvalid,
+				StatusCode::BAD_REQUEST,
+				oci_spec::distribution::ErrorCode::ManifestInvalid,
 				"Content-Type header is required".to_string(),
 			)
 		})?
 		.to_str()
 		.map_err(internal_server_error_response)?;
 
-	if header_content_type != "application/octet-stream" {
+	if !(content_type == "application/vnd.oci.image.manifest.v1+json") {
 		return Err(convert_oci_error(
 			StatusCode::BAD_REQUEST,
-			ErrorCode::BlobUploadInvalid,
-			format!(
-				"Content-Type must be application/octet-stream, got {}",
-				header_content_type
-			),
+			oci_spec::distribution::ErrorCode::ManifestInvalid,
+			"Unsupported Content-Type".to_string(),
 		));
 	}
 
@@ -112,9 +84,8 @@ pub(super) async fn handle(
 		.compat();
 
 	let bucket = get_s3_bucket(state.config.clone())?;
-	let s3_key = get_s3_object_name_for_blob(&digest);
+	let s3_key = get_s3_object_name_for_manifest(&referrer);
 
-	// TODO match the content length
 	let status = bucket
 		.put_object_stream_with_content_type(&mut body_stream, s3_key, "application/octet-stream")
 		.await
@@ -128,22 +99,16 @@ pub(super) async fn handle(
 		));
 	}
 
-	query!(
-		r#"
-		INSERT INTO container_registry_layer_blob(
-			digest,
-			size
-		)
-		VALUES ($1, $2);
-		"#,
-		&digest,
-		header_content_length
-			.parse::<i64>()
-			.map_err(internal_server_error_response)?
-	)
-	.execute(&mut *database)
-	.await
-	.map_err(internal_server_error_response)?;
+	// TODO: This also needs to be completed, but rn I don't have the right
+	// column
+	// query!(
+	//     r#"
+	//         INSERT INTO
+	//     "#
+	// )
+	// .execute(&mut *database)
+	// .await
+	// .map_err(internal_server_error_response)?;
 
 	let headers = [
 		(
@@ -151,14 +116,10 @@ pub(super) async fn handle(
 			Some(String::from("registry/2.0")),
 		),
 		(
-			HeaderName::from_static("Docker-Content-Digest"),
-			Some(digest.to_string()),
-		),
-		(
 			HeaderName::from_static("Location"),
 			Some(format!(
-				"https://registry.patr.cloud/v2/{}/{}/blobs/{}",
-				path.workspace_id, path.repo_name, digest
+				"https://registry.patr.cloud/v2/{}/{}/manifests/{}",
+				path.workspace_id, path.repo_name, referrer
 			)),
 		),
 	]
