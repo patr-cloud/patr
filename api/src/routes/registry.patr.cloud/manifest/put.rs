3776d69f4340997a04e3a17cc68312a740a53230
Argha Chakrabarty <argha.chakrabarty@girnarsoft.com>
2025-09-01 11:43:59 +0000

    add get/head routes for manifest/reference

---
+++ api/src/routes/registry.patr.cloud/manifest/put.rs
@@ -0,0 +1,206 @@
+use axum::{
+	body::Body,
+	extract::{Path, State},
+	http::{HeaderMap, StatusCode},
+	response::IntoResponse,
+};
+use futures::TryStreamExt;
+use oci_spec::distribution::ErrorCode;
+use serde::{Deserialize, Serialize};
+use tokio_util::compat::FuturesAsyncReadCompatExt;
+
+use super::super::Error;
+use crate::{
+	prelude::*,
+	routes::registry_patr_cloud::{
+		get_s3_object_name_for_manifest,
+		internal_server_error_response,
+	},
+	utils::helper::{
+		Referrer,
+		check_workspace,
+		convert_oci_error,
+		get_header,
+		get_referrer,
+		get_s3_bucket,
+		preprocess_stuff,
+	},
+};
+
+#[preprocess::sync]
+/// The parameters that are passed in the path of the request
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PathParams {
+	/// The workspace ID of the repository
+	workspace_id: Uuid,
+	/// The name of the repository
+	#[preprocess(regex = r"[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*")]
+	repo_name: String,
+	/// The digest/tag of the blob
+	#[preprocess(trim)]
+	referrer: String,
+}
+
+/// Handles the `GET /v2/<name>/manifests/<reference>` route. i.e. Pushing a manifest. See [end-7](https://github.com/opencontainers/distribution-spec/blob/main/spec.md#endpoints) for more details
+#[axum::debug_handler]
+pub(super) async fn handle(
+	header: HeaderMap,
+	Path(path): Path<PathParams>,
+	State(state): State<AppState>,
+	body: Body,
+) -> Result<impl IntoResponse, Error> {
+	let path = preprocess_stuff(path)?;
+
+	let workspace_id = path.workspace_id;
+	check_workspace(workspace_id, state.clone()).await?;
+	let mut database = state
+		.database
+		.begin()
+		.await
+		.map_err(internal_server_error_response)?;
+
+	let content_type = get_header(&header, "Content-Type")?;
+	if !(content_type == "application/vnd.oci.image.manifest.v1+json") {
+		return Err(convert_oci_error(
+			StatusCode::BAD_REQUEST,
+			oci_spec::distribution::ErrorCode::ManifestInvalid,
+			"Unsupported Content-Type".to_string(),
+		));
+	}
+
+	let referrer = get_referrer(&path.referrer);
+	let manifest = match referrer {
+		Referrer::Digest(digest) => query!(
+			r#"
+				SELECT
+					mani.digest,
+					mani.size
+				FROM
+					container_registry_manifest AS mani
+				WHERE
+					digest = $1;
+				"#,
+			digest
+		)
+		.fetch_optional(&mut *database)
+		.await
+		.map_err(internal_server_error_response)?
+		.map(|mani| (mani.digest, mani.size)),
+		Referrer::Tag(tag) => query!(
+			r#"
+				SELECT 
+					mani.digest,
+					mani.size 
+				FROM 
+					container_registry_manifest AS mani
+				INNER JOIN
+					container_registry_tag AS tag
+				ON
+					mani.digest = tag.manifest_digest
+				WHERE
+					tag.name = $1;
+				"#,
+			tag
+		)
+		.fetch_optional(&mut *database)
+		.await
+		.map_err(internal_server_error_response)?
+		.map(|mani| (mani.digest, mani.size)),
+	};
+
+	let (digest, size) = manifest.ok_or_else(|| {
+		convert_oci_error(
+			StatusCode::NOT_FOUND,
+			ErrorCode::ManifestUnknown,
+			"Manifest not found".to_string(),
+		)
+	})?;
+
+	if !(200..300).contains(&status.status_code()) {
+		return Err(convert_oci_error(
+			StatusCode::BAD_REQUEST,
+			ErrorCode::ManifestInvalid,
+			"Failed to push manifest to S3".to_string(),
+		));
+	}
+
+	let index_manifest_digest = match referrer {
+		Referrer::Digest(digest) => query!(
+			r#"
+				SELECT
+					digest
+				FROM
+					container_registry_manifest_index AS index
+				INNER JOIN
+					container_registry_manifest AS mani
+				ON
+					index.digest = mani.index_digest
+				WHERE
+					digest = $1
+				;
+				"#,
+			digest
+		)
+		.fetch_optional(&mut *database)
+		.await
+		.map_err(internal_server_error_response)?
+		.map(|rec| rec.digest),
+		Referrer::Tag(tag) => {}
+	};
+
+	let index_manifest = query!(
+		r#"
+		SELECT
+			digest
+		FROM
+			container_registry_manifest_index
+		;
+		"#
+	)
+	.fetch_optional(&mut *database)
+	.await
+	.map_err(internal_server_error_response)?
+	.map(|rec| rec.digest);
+
+	if (index_manifest.is_none()) {
+		query!(
+			r#"
+				INSERT INTO
+					container_registry_manifest_index (digest)
+				VALUES ($1)
+			"#,
+			digest
+		)
+		.execute(&mut *database)
+		.await
+		.map_err(internal_server_error_response)?;
+	}
+
+	let mut body_stream = body
+		.into_data_stream()
+		.map_err(std::io::Error::other)
+		.into_async_read()
+		.compat();
+
+	let bucket = get_s3_bucket(state.config.clone())?;
+	let s3_key = get_s3_object_name_for_manifest(&digest);
+
+	let status = bucket
+		.put_object_stream_with_content_type(&mut body_stream, s3_key, "application/octet-stream")
+		.await
+		.map_err(internal_server_error_response)?;
+
+	let headers = [
+		("Docker-Distribution-API-Version", "registry/2.0"),
+		(
+			"Location",
+			&format!(
+				"https://registry.patr.cloud/v2/{}/{}/manifests/{}",
+				path.workspace_id, path.repo_name, &digest
+			),
+		),
+	];
+
+	Ok((StatusCode::OK, headers).into_response())
+}
---
+++ api/src/routes/registry.patr.cloud/manifest/reference.rs
@@ -0,0 +1,145 @@
+use axum::{
+	body::Body,
+	extract::{Path, State},
+	http::{Method, StatusCode},
+	response::IntoResponse,
+};
+use oci_spec::distribution::ErrorCode;
+use serde::{Deserialize, Serialize};
+
+use crate::{
+	prelude::*,
+	routes::registry_patr_cloud::{
+		Error,
+		get_s3_object_name_for_manifest,
+		internal_server_error_response,
+	},
+	utils::helper::{
+		Referrer,
+		check_workspace,
+		convert_oci_error,
+		get_referrer,
+		get_s3_bucket,
+		preprocess_stuff,
+	},
+};
+
+#[preprocess::sync]
+/// The parameters that are passed in the path of the request
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct PathParams {
+	/// The workspace ID of the repository
+	workspace_id: Uuid,
+	/// The name of the repository
+	#[preprocess(regex = r"[a-z0-9]+((\.|_|__|-+)[a-z0-9]+)*")]
+	repo_name: String,
+	/// The digest of the blob
+	#[preprocess(trim)]
+	referrer: String,
+}
+
+/// Handles the `HEAD /v2/v2/<name>/manifests/<reference>` route. [`end-3`](https://github.com/opencontainers/distribution-spec/blob/main/spec.md#pulling-blobs)
+#[axum::debug_handler]
+pub(super) async fn handle(
+	method: Method,
+	Path(path): Path<PathParams>,
+	State(state): State<AppState>,
+) -> Result<impl IntoResponse, Error> {
+	let path = preprocess_stuff(path)?;
+
+	let workspace_id = path.workspace_id;
+	check_workspace(workspace_id, state.clone()).await?;
+
+	let referrer = get_referrer(&path.referrer);
+	let mut database = state
+		.database
+		.begin()
+		.await
+		.map_err(internal_server_error_response)?;
+
+	let manifest = match referrer {
+		Referrer::Digest(digest) => query!(
+			r#"
+				SELECT
+					mani.digest,
+					mani.size
+				FROM
+					container_registry_manifest AS mani
+				WHERE
+					digest = $1;
+				"#,
+			digest
+		)
+		.fetch_optional(&mut *database)
+		.await
+		.map_err(internal_server_error_response)?
+		.map(|mani| (mani.digest, mani.size)),
+		Referrer::Tag(tag) => query!(
+			r#"
+				SELECT 
+					mani.digest,
+					mani.size 
+				FROM 
+					container_registry_manifest AS mani
+				INNER JOIN
+					container_registry_tag AS tag
+				ON
+					mani.digest = tag.manifest_digest
+				WHERE
+					tag.name = $1;
+				"#,
+			tag
+		)
+		.fetch_optional(&mut *database)
+		.await
+		.map_err(internal_server_error_response)?
+		.map(|mani| (mani.digest, mani.size)),
+	};
+
+	let (digest, size) = manifest.ok_or_else(|| {
+		convert_oci_error(
+			StatusCode::NOT_FOUND,
+			ErrorCode::ManifestUnknown,
+			"Manifest not found".to_string(),
+		)
+	})?;
+
+	if matches!(method, Method::HEAD) {
+		// HEAD request. just set the headers
+		return Ok((
+			StatusCode::OK,
+			[
+				("Docker-Distribution-API-Version", "registry/2.0"),
+				("Docker-Content-Digest", &digest),
+				("Content-Length", &size.to_string()),
+			],
+		)
+			.into_response());
+	} else {
+		let bucket = get_s3_bucket(state.config.clone())?;
+		let s3_key = get_s3_object_name_for_manifest(&digest);
+		let object = bucket
+			.get_object_stream(&s3_key)
+			.await
+			.map_err(internal_server_error_response)?;
+
+		if !(200..300).contains(&object.status_code) {
+			return Err(convert_oci_error(
+				StatusCode::NOT_FOUND,
+				ErrorCode::BlobUnknown,
+				"Blob not found".to_string(),
+			));
+		}
+
+		return Ok((
+			StatusCode::OK,
+			[
+				("Docker-Distribution-API-Version", "registry/2.0"),
+				("Docker-Content-Digest", &digest),
+			],
+			Body::from_stream(object.bytes),
+		)
+			.into_response());
+	}
+}
