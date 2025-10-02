9c9d62560c8090778b99bed0e0dc33a4fb69f2a8
Argha Chakrabarty <argha.chakrabarty@girnarsoft.com>
2025-10-02 16:09:45 +0000

    remove frontend

--- frontend/common/src/pages/mod.rs
+++
@@ -1,8 +0,0 @@
-/// The content to show when the user is logged in
-mod logged_in_content;
-/// The content to show when the user is not logged in
-mod logged_out_content;
-/// The 404 not found page when no other route is matched
-mod not_found;
-
-pub use self::{logged_in_content::*, logged_out_content::*, not_found::*};
